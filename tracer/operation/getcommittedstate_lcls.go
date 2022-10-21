3843ea3cb499cc89df0432dbba02deca3f8d82c8
Bernhard Scholz <bernhard@fantom.foundation>
2022-10-21 16:05:01 +1100

    Add new compressed GetCommittedState Operation
    This new operation compresses GetCommittedState so that it refers to the last
    contract and last storage addresses without writing these addresses
    to the file.

---
+++ tracer/operation/getcommittedstate_lcls.go
@@ -0,0 +1,47 @@
+package operation
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Fantom-foundation/Aida/tracer/dict"
+	"github.com/Fantom-foundation/Aida/tracer/state"
+)
+
+// Get-state data structure for last contract and last storage address.
+type GetCommittedStateLcls struct {
+}
+
+// Return the get-committed-state-lcls operation identifier.
+func (op *GetCommittedStateLcls) GetOpId() byte {
+	return GetCommittedStateLclsID
+}
+
+// Create a new get-committed-state-lcls operation.
+func NewGetCommittedStateLcls() *GetCommittedStateLcls {
+	return new(GetCommittedStateLcls)
+}
+
+// Read a get-committed-state-lcls operation from a file.
+func ReadGetCommittedStateLcls(file *os.File) (Operation, error) {
+	return NewGetCommittedStateLcls(), nil
+}
+
+// Write the get-committed-state-lcls operation to file.
+func (op *GetCommittedStateLcls) Write(f *os.File) error {
+	return nil
+}
+
+// Execute the get-committed-state-lcls operation.
+func (op *GetCommittedStateLcls) Execute(db state.StateDB, ctx *dict.DictionaryContext) {
+	contract := ctx.LastContractAddress()
+	storage := ctx.LookupStorage(0)
+	db.GetCommittedState(contract, storage)
+}
+
+// Print a debug message for get-committed-state-lcls operation.
+func (op *GetCommittedStateLcls) Debug(ctx *dict.DictionaryContext) {
+	contract := ctx.LastContractAddress()
+	storage := ctx.ReadStorage(0)
+	fmt.Printf("\tcontract: %v\t storage: %v\n", contract, storage)
+}
