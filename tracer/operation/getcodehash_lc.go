23a7a0e8a83ccbe7207f5f36a7b89236aa0c7387
wsodsong <wasuwee.sodsong@gmail.com>
2022-10-21 12:10:23 +0000

    Adds new compressed GetCodeHash operation
    New operation GetCodeHashLc executes GetCodeHash with the most recently
    used contract address without writing the address to the trace file.
    
    This new operation replay roughly 50% of GetCodeHash.

---
+++ tracer/operation/getcodehash_lc.go
@@ -0,0 +1,45 @@
+package operation
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Fantom-foundation/Aida/tracer/dict"
+	"github.com/Fantom-foundation/Aida/tracer/state"
+)
+
+// Get-code-hash data structure with last contract address
+type GetCodeHashLc struct {
+}
+
+// Return the get-code-hash-lc operation identifier.
+func (op *GetCodeHashLc) GetOpId() byte {
+	return GetCodeHashLcID
+}
+
+// Create a new get-code-hash-lc operation.
+func NewGetCodeHashLc() *GetCodeHashLc {
+	return &GetCodeHashLc{}
+}
+
+// Read a get-code-hash-lc operation from a file.
+func ReadGetCodeHashLc(file *os.File) (Operation, error) {
+	return NewGetCodeHashLc(), nil
+}
+
+// Write the get-code-hash-lc operation to a file.
+func (op *GetCodeHashLc) Write(f *os.File) error {
+	return nil
+}
+
+// Execute the get-code-hash-lc operation.
+func (op *GetCodeHashLc) Execute(db state.StateDB, ctx *dict.DictionaryContext) {
+	contract := ctx.LastContractAddress()
+	db.GetCodeHash(contract)
+}
+
+// Print a debug message for get-code-hash-lc.
+func (op *GetCodeHashLc) Debug(ctx *dict.DictionaryContext) {
+	contract := ctx.LastContractAddress()
+	fmt.Printf("\tcontract: %v\n", contract)
+}
