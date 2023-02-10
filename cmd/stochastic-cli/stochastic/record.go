69625167ba47b50244e59662d83e8631ac09cfc3
wsodsong <wasuwee.sodsong@gmail.com>
2023-02-10 14:06:51 +0000

    Refactor Tx processing function
    Refactoring of a common tx processing code as a utility function.
    
    Items included in this PR:
    - add a single Tx processing function used by  `record`, `aida-runvm` and
      `aida-runarchive`
    - improve an error message handling, esp in the case of continue on
      failure
    - record commands adopt Config struct for handling cli options
    - add unittests

--- cmd/stochastic-cli/stochastic/record.go
+++ cmd/stochastic-cli/stochastic/record.go
@@ -5,24 +5,14 @@ import (
 	"fmt"
 	"log"
 	"math"
-	"math/big"
 	"os"
 	"runtime/pprof"
 	"time"
 
-	"github.com/Fantom-foundation/Aida/cmd/substate-cli/replay"
 	"github.com/Fantom-foundation/Aida/state"
 	"github.com/Fantom-foundation/Aida/stochastic"
 	"github.com/Fantom-foundation/Aida/utils"
 	substate "github.com/Fantom-foundation/Substate"
-	"github.com/Fantom-foundation/go-opera/evmcore"
-	"github.com/Fantom-foundation/go-opera/opera"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/urfave/cli/v2"
 )
 
@@ -49,137 +39,17 @@ The stochastic record command requires two arguments:
 last block for recording events.`,
 }
 
-// stochasticRecordTask generates events for a transaction.
-func stochasticRecordTask(block uint64, tx, chainID int, recording *substate.Substate, eventRegistry *stochastic.EventRegistry) error {
-
-	inputAlloc := recording.InputAlloc
-	inputEnv := recording.Env
-	inputMessage := recording.Message
-
-	outputAlloc := recording.OutputAlloc
-	outputResult := recording.Result
-
-	var (
-		vmConfig    vm.Config
-		chainConfig *params.ChainConfig
-	)
-
-	vmConfig = opera.DefaultVMConfig
-	vmConfig.NoBaseFee = true
-	chainConfig = utils.GetChainConfig(chainID)
-
-	var hashError error
-	getHash := func(num uint64) common.Hash {
-		if inputEnv.BlockHashes == nil {
-			hashError = fmt.Errorf("getHash(%d) invoked, no blockhashes provided", num)
-			return common.Hash{}
-		}
-		h, ok := inputEnv.BlockHashes[num]
-		if !ok {
-			hashError = fmt.Errorf("getHash(%d) invoked, blockhash for that block not provided", num)
-		}
-		return h
-	}
-
-	var statedb state.StateDB
-	statedb = state.MakeGethInMemoryStateDB(&inputAlloc, inputEnv.Number)
-	statedb = stochastic.NewEventProxy(statedb, eventRegistry)
-
-	// Apply Message
-	var (
-		gaspool   = new(evmcore.GasPool)
-		blockHash = common.Hash{0x01}
-		txHash    = common.Hash{0x02}
-		txIndex   = tx
-	)
-
-	gaspool.AddGas(inputEnv.GasLimit)
-	blockCtx := vm.BlockContext{
-		CanTransfer: core.CanTransfer,
-		Transfer:    core.Transfer,
-		Coinbase:    inputEnv.Coinbase,
-		BlockNumber: new(big.Int).SetUint64(inputEnv.Number),
-		Time:        new(big.Int).SetUint64(inputEnv.Timestamp),
-		Difficulty:  inputEnv.Difficulty,
-		GasLimit:    inputEnv.GasLimit,
-		GetHash:     getHash,
-	}
-	// If currentBaseFee is defined, add it to the vmContext.
-	if inputEnv.BaseFee != nil {
-		blockCtx.BaseFee = new(big.Int).Set(inputEnv.BaseFee)
-	}
-
-	msg := inputMessage.AsMessage()
-
-	statedb.Prepare(txHash, txIndex)
-
-	txCtx := evmcore.NewEVMTxContext(msg)
-
-	evm := vm.NewEVM(blockCtx, txCtx, statedb, chainConfig, vmConfig)
-
-	snapshot := statedb.Snapshot()
-
-	msgResult, err := evmcore.ApplyMessage(evm, msg, gaspool)
-	if err != nil {
-		statedb.RevertToSnapshot(snapshot)
-		return err
-	}
-	if hashError != nil {
-		return hashError
-	}
-	if chainConfig.IsByzantium(blockCtx.BlockNumber) {
-		statedb.Finalise(true)
-	} else {
-		statedb.IntermediateRoot(chainConfig.IsEIP158(blockCtx.BlockNumber))
-	}
-
-	evmResult := &substate.SubstateResult{}
-	if msgResult.Failed() {
-		evmResult.Status = types.ReceiptStatusFailed
-	} else {
-		evmResult.Status = types.ReceiptStatusSuccessful
-	}
-	evmResult.Logs = statedb.GetLogs(txHash, blockHash)
-	evmResult.Bloom = types.BytesToBloom(types.LogsBloom(evmResult.Logs))
-	if to := msg.To(); to == nil {
-		evmResult.ContractAddress = crypto.CreateAddress(evm.TxContext.Origin, msg.Nonce())
-	}
-	evmResult.GasUsed = msgResult.UsedGas
-	evmAlloc := statedb.GetSubstatePostAlloc()
-
-	r := outputResult.Equal(evmResult)
-	a := outputAlloc.Equal(evmAlloc)
-	if !(r && a) {
-		fmt.Printf("Block: %v Transaction: %v\n", block, tx)
-		if !r {
-			fmt.Printf("inconsistent output: result\n")
-			replay.PrintResultDiffSummary(outputResult, evmResult)
-		}
-		if !a {
-			fmt.Printf("inconsistent output: alloc\n")
-			replay.PrintAllocationDiffSummary(&outputAlloc, &evmAlloc)
-		}
-		return fmt.Errorf("inconsistent output")
-	}
-
-	return nil
-}
-
 // stochasticRecordAction implements recording of events.
 func stochasticRecordAction(ctx *cli.Context) error {
 	substate.RecordReplay = true
 	var err error
 
-	if ctx.Args().Len() != 2 {
-		return fmt.Errorf("stochastic record command requires exactly 2 arguments")
+	cfg, err := utils.NewConfig(ctx, utils.BlockRangeArgs)
+	if err != nil {
+		return err
 	}
-
-	// Fetch length of epoch from command line.
-	epochLength := ctx.Uint64(utils.EpochLengthFlag.Name)
-	if epochLength <= 0 {
-		epochLength = 300
-	}
-	log.Printf("Using epoch length of %d blocks\n", epochLength)
+	// force enable tracsaction validation
+	cfg.ValidateTxState = true
 
 	// start CPU profiling if enabled.
 	if profileFileName := ctx.String(utils.CpuProfileFlag.Name); profileFileName != "" {
@@ -191,24 +61,15 @@ func stochasticRecordAction(ctx *cli.Context) error {
 		defer pprof.StopCPUProfile()
 	}
 
-	// get progress flag
-	enableProgress := !ctx.Bool(utils.DisableProgressFlag.Name)
-
-	// process arguments
-	chainID := ctx.Int(utils.ChainIDFlag.Name)
 	if ctx.Bool(utils.TraceDebugFlag.Name) {
 		utils.TraceDebug = true
-	}
-	first, last, argErr := utils.SetBlockRange(ctx.Args().Get(0), ctx.Args().Get(1))
-	if argErr != nil {
-		return argErr
 	}
 
 	// iterate through subsets in sequence
 	substate.SetSubstateFlags(ctx)
 	substate.OpenSubstateDBReadOnly()
 	defer substate.CloseSubstateDB()
-	iter := substate.NewSubstateIterator(first, ctx.Int(substate.WorkersFlag.Name))
+	iter := substate.NewSubstateIterator(cfg.First, ctx.Int(substate.WorkersFlag.Name))
 	defer iter.Release()
 	oldBlock := uint64(math.MaxUint64) // set to an infeasible block
 	var (
@@ -216,7 +77,7 @@ func stochasticRecordAction(ctx *cli.Context) error {
 		sec     float64
 		lastSec float64
 	)
-	if enableProgress {
+	if cfg.EnableProgress {
 		start = time.Now()
 		sec = time.Since(start).Seconds()
 		lastSec = time.Since(start).Seconds()
@@ -225,7 +86,7 @@ func stochasticRecordAction(ctx *cli.Context) error {
 	// create a new event registry
 	eventRegistry := stochastic.NewEventRegistry()
 
-	curEpoch := first / epochLength
+	curEpoch := cfg.First / cfg.EpochLength
 	eventRegistry.RegisterOp(stochastic.BeginEpochID)
 
 	// iterate over all substates in order
@@ -233,12 +94,12 @@ func stochasticRecordAction(ctx *cli.Context) error {
 		tx := iter.Value()
 		// close off old block with an end-block operation
 		if oldBlock != tx.Block {
-			if tx.Block > last {
+			if tx.Block > cfg.Last {
 				break
 			}
 			if oldBlock != math.MaxUint64 {
 				eventRegistry.RegisterOp(stochastic.EndBlockID)
-				newEpoch := tx.Block / epochLength
+				newEpoch := tx.Block / cfg.EpochLength
 				for curEpoch < newEpoch {
 					eventRegistry.RegisterOp(stochastic.EndEpochID)
 					curEpoch++
@@ -250,9 +111,16 @@ func stochasticRecordAction(ctx *cli.Context) error {
 			oldBlock = tx.Block
 		}
 		eventRegistry.RegisterOp(stochastic.BeginTransactionID)
-		stochasticRecordTask(tx.Block, tx.Transaction, chainID, tx.Substate, &eventRegistry)
+
+		var statedb state.StateDB
+		statedb = state.MakeGethInMemoryStateDB(&tx.Substate.InputAlloc, tx.Block)
+		statedb = stochastic.NewEventProxy(statedb, &eventRegistry)
+		if err := utils.ProcessTx(statedb, cfg, tx.Block, tx.Transaction, tx.Substate); err != nil {
+			return err
+		}
+
 		eventRegistry.RegisterOp(stochastic.EndTransactionID)
-		if enableProgress {
+		if cfg.EnableProgress {
 			// report progress
 			sec = time.Since(start).Seconds()
 			if sec-lastSec >= 15 {
@@ -267,9 +135,9 @@ func stochasticRecordAction(ctx *cli.Context) error {
 	}
 	eventRegistry.RegisterOp(stochastic.EndEpochID)
 
-	if enableProgress {
+	if cfg.EnableProgress {
 		sec = time.Since(start).Seconds()
-		fmt.Printf("stochastic record: Total elapsed time: %.3f s, processed %v blocks\n", sec, last-first+1)
+		fmt.Printf("stochastic record: Total elapsed time: %.3f s, processed %v blocks\n", sec, cfg.Last-cfg.First+1)
 	}
 
 	// writing event registry
--- cmd/substate-cli/replay/replay.go
+++ cmd/substate-cli/replay/replay.go
@@ -1,7 +1,6 @@
 package replay
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"math/big"
@@ -11,14 +10,13 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/Fantom-foundation/Aida/state"
 	"github.com/Fantom-foundation/Aida/utils"
 	substate "github.com/Fantom-foundation/Substate"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-
-	"github.com/Fantom-foundation/Aida/state"
 	"github.com/Fantom-foundation/go-opera/evmcore"
 	"github.com/Fantom-foundation/go-opera/opera"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/core/vm/lfvm"
@@ -215,123 +213,16 @@ func replayTask(config ReplayConfig, block uint64, tx int, recording *substate.S
 		fmt.Printf("block: %v Transaction: %v\n", block, tx)
 		if !r {
 			fmt.Printf("inconsistent output: result\n")
-			PrintResultDiffSummary(outputResult, evmResult)
+			utils.PrintResultDiffSummary(outputResult, evmResult)
 		}
 		if !a {
 			fmt.Printf("inconsistent output: alloc\n")
-			PrintAllocationDiffSummary(&outputAlloc, &evmAlloc)
+			utils.PrintAllocationDiffSummary(&outputAlloc, &evmAlloc)
 		}
 		return fmt.Errorf("inconsistent output")
 	}
 
 	return nil
-}
-
-func printIfDifferent[T comparable](label string, want, have T) bool {
-	if want != have {
-		fmt.Printf("  Different %s:\n", label)
-		fmt.Printf("    want: %v\n", want)
-		fmt.Printf("    have: %v\n", have)
-		return true
-	}
-	return false
-}
-
-func printIfDifferentBytes(label string, want, have []byte) bool {
-	if !bytes.Equal(want, have) {
-		fmt.Printf("  Different %s:\n", label)
-		fmt.Printf("    want: %v\n", want)
-		fmt.Printf("    have: %v\n", have)
-		return true
-	}
-	return false
-}
-
-func printIfDifferentBigInt(label string, want, have *big.Int) bool {
-	if want == nil && have == nil {
-		return false
-	}
-	if want == nil || have == nil || want.Cmp(have) != 0 {
-		fmt.Printf("  Different %s:\n", label)
-		fmt.Printf("    want: %v\n", want)
-		fmt.Printf("    have: %v\n", have)
-		return true
-	}
-	return false
-}
-
-func PrintResultDiffSummary(want, have *substate.SubstateResult) {
-	printIfDifferent("status", want.Status, have.Status)
-	printIfDifferent("contract address", want.ContractAddress, have.ContractAddress)
-	printIfDifferent("gas usage", want.GasUsed, have.GasUsed)
-	printIfDifferent("log bloom filter", want.Bloom, have.Bloom)
-	if !printIfDifferent("log size", len(want.Logs), len(have.Logs)) {
-		for i := range want.Logs {
-			printLogDiffSummary(fmt.Sprintf("log[%d]", i), want.Logs[i], have.Logs[i])
-		}
-	}
-}
-
-func printLogDiffSummary(label string, want, have *types.Log) {
-	printIfDifferent(fmt.Sprintf("%s.address", label), want.Address, have.Address)
-	if !printIfDifferent(fmt.Sprintf("%s.Topics size", label), len(want.Topics), len(have.Topics)) {
-		for i := range want.Topics {
-			printIfDifferent(fmt.Sprintf("%s.Topics[%d]", label, i), want.Topics[i], have.Topics[i])
-		}
-	}
-	printIfDifferentBytes(fmt.Sprintf("%s.data", label), want.Data, have.Data)
-}
-
-func PrintAllocationDiffSummary(want, have *substate.SubstateAlloc) {
-	printIfDifferent("substate alloc size", len(*want), len(*have))
-	for key := range *want {
-		_, present := (*have)[key]
-		if !present {
-			fmt.Printf("    missing key=%v\n", key)
-		}
-	}
-
-	for key := range *have {
-		_, present := (*want)[key]
-		if !present {
-			fmt.Printf("    extra key=%v\n", key)
-		}
-	}
-
-	for key, is := range *have {
-		should, present := (*want)[key]
-		if present {
-			printAccountDiffSummary(fmt.Sprintf("key=%v:", key), should, is)
-		}
-	}
-}
-
-func printAccountDiffSummary(label string, want, have *substate.SubstateAccount) {
-	printIfDifferent(fmt.Sprintf("%s.Nonce", label), want.Nonce, have.Nonce)
-	printIfDifferentBigInt(fmt.Sprintf("%s.Balance", label), want.Balance, have.Balance)
-	printIfDifferentBytes(fmt.Sprintf("%s.Code", label), want.Code, have.Code)
-
-	printIfDifferent(fmt.Sprintf("len(%s.Storage)", label), len(want.Storage), len(have.Storage))
-	for key := range want.Storage {
-		_, present := have.Storage[key]
-		if !present {
-			fmt.Printf("    %s.Storage misses key %v\n", label, key)
-		}
-	}
-
-	for key := range have.Storage {
-		_, present := want.Storage[key]
-		if !present {
-			fmt.Printf("    %s.Storage has extra key %v\n", label, key)
-		}
-	}
-
-	for key, is := range have.Storage {
-		should, present := want.Storage[key]
-		if present {
-			printIfDifferent(fmt.Sprintf("%s.Storage[%v]", label, key), should, is)
-		}
-	}
 }
 
 // create new execution context for a data collector
