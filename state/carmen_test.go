d29317dc8552c18821f48fae18270ac4bfbd6835
Lubomír Jahn <lubomir.j@icloud.com>
2023-02-27 07:33:43 +0100

    Fix state/carmen_test import formating

--- state/carmen_test.go
+++ state/carmen_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type testCase struct {
