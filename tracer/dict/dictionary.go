741ed2652d8e1844cf393e6c858c7f26aea4f11e
Bernhard Scholz <bernhard@fantom.foundation>
2023-02-27 20:04:13 +1100

    Refactor

--- tracer/dict/dictionary.go
+++ tracer/dict/dictionary.go
@@ -6,28 +6,26 @@ import (
 	"io"
 	"math"
 	"os"
-	"reflect"
 
 	"github.com/dsnet/compress/bzip2"
 )
 
-// DictionaryLimit sets size of value dictionary.
+// DictionaryLimit sets size of dictionary.
 var DictionaryLimit int = math.MaxInt - 1
 
-// Dictionary data structure envalues/devalues a value
-// to an index or vice versa.
+// Dictionary data structure encodes/decodes a value to an index or vice versa.
 type Dictionary[K comparable] struct {
 	valueToIdx map[K]int // value to index map for encoding
 	idxToValue []K       // value array for decoding
 }
 
-// Init initializes or clears a value dictionary.
+// Init initializes or clears a dictionary.
 func (d *Dictionary[K]) Init() {
 	d.valueToIdx = map[K]int{}
 	d.idxToValue = []K{}
 }
 
-// NewDictionary creates a new value dictionary.
+// NewDictionary creates a new dictionary.
 func NewDictionary[K comparable]() *Dictionary[K] {
 	p := new(Dictionary[K])
 	p.Init()
@@ -41,7 +39,7 @@ func (d *Dictionary[K]) Encode(value K) (int, error) {
 	if !ok {
 		idx = len(d.idxToValue)
 		if idx >= DictionaryLimit {
-			return 0, fmt.Errorf("Value dictionary exhausted")
+			return 0, fmt.Errorf("dictionary exhausted")
 		}
 		d.valueToIdx[value] = idx
 		d.idxToValue = append(d.idxToValue, value)
@@ -61,14 +59,14 @@ func (d *Dictionary[K]) Decode(idx int) (K, error) {
 
 // Write dictionary to a binary file.
 func (d *Dictionary[K]) Write(filename string, magic uint64) error {
-	// open value dictionary file for writing
+	// open dictionary file for writing
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
-		return fmt.Errorf("Cannot open value-dictionary file. Error:%v", err)
+		return fmt.Errorf("Cannot open dictionary file. Error:%v", err)
 	}
 	zfile, err := bzip2.NewWriter(file, &bzip2.WriterConfig{Level: 9})
 	if err != nil {
-		return fmt.Errorf("Cannot open bzip2 stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot open bzip2 stream of dictionary. Error: %v", err)
 	}
 	// write magic number
 	if err := binary.Write(zfile, binary.LittleEndian, &magic); err != nil {
@@ -82,39 +80,31 @@ func (d *Dictionary[K]) Write(filename string, magic uint64) error {
 	}
 	// close file
 	if err := zfile.Close(); err != nil {
-		return fmt.Errorf("Cannot close bzip2 stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot close bzip2 stream of dictionary. Error: %v", err)
 	}
 	if err := file.Close(); err != nil {
-		return fmt.Errorf("Cannot close value-dictionary file. Error: %v", err)
+		return fmt.Errorf("Cannot close dictionary file. Error: %v", err)
 	}
 	return nil
 }
 
-// super slow
-func mylen[K any](x K) int {
-	if reflect.TypeOf(any(x)).Name() == "string" {
-		return len(any(x).(string))
-	}
-	return 0
-}
-
 // Read dictionary from a binary file.
 func (d *Dictionary[K]) Read(filename string, magic uint64) error {
-	// clear value dictionary
+	// clear dictionary
 	d.Init()
-	// open value dictionary file for reading, read buffer, and bzip2 stream
+	// open dictionary file for reading, read buffer, and bzip2 stream
 	file, err := os.Open(filename)
 	if err != nil {
-		return fmt.Errorf("Cannot open value-dictionary file. Error:%v", err)
+		return fmt.Errorf("Cannot open dictionary file. Error:%v", err)
 	}
 	zfile, err := bzip2.NewReader(file, &bzip2.ReaderConfig{})
 	if err != nil {
-		return fmt.Errorf("Cannot open bzip stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot open bzip stream of dictionary. Error: %v", err)
 	}
 	// read and check magic number
 	var magicData uint64
 	if err := binary.Read(zfile, binary.LittleEndian, &magicData); err != nil && magic != magicData {
-		return fmt.Errorf("Cannot read magic number; value-dictionary is corrupted. Error: %v", err)
+		return fmt.Errorf("Cannot read magic number; dictionary is corrupted. Error: %v", err)
 	}
 	// read entries from file
 	var data K
@@ -126,12 +116,12 @@ func (d *Dictionary[K]) Read(filename string, magic uint64) error {
 			}
 			return fmt.Errorf("Error reading storage address. Error: %v", err)
 		}
-		// envalue entry
+		// encode entry
 		idx, err := d.Encode(data)
 		if err != nil {
 			return err
 		} else if idx != ctr {
-			return fmt.Errorf("Corrupted value dictionary file entries")
+			return fmt.Errorf("Corrupted dictionary file entries")
 		}
 	}
 	// close file
@@ -146,14 +136,14 @@ func (d *Dictionary[K]) Read(filename string, magic uint64) error {
 
 // Write dictionary to a binary file.
 func (d *Dictionary[K]) WriteString(filename string, magic uint64) error {
-	// open value dictionary file for writing
+	// open dictionary file for writing
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
-		return fmt.Errorf("Cannot open value-dictionary file. Error: %v", err)
+		return fmt.Errorf("Cannot open dictionary file. Error: %v", err)
 	}
 	zfile, err := bzip2.NewWriter(file, &bzip2.WriterConfig{Level: 9})
 	if err != nil {
-		return fmt.Errorf("Cannot open bzip2 stream for value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot open bzip2 stream for dictionary. Error: %v", err)
 	}
 	// write magic number
 	if err := binary.Write(zfile, binary.LittleEndian, &magic); err != nil {
@@ -177,31 +167,31 @@ func (d *Dictionary[K]) WriteString(filename string, magic uint64) error {
 	}
 	// close file
 	if err := zfile.Close(); err != nil {
-		return fmt.Errorf("Cannot close bzip2 stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot close bzip2 stream of dictionary. Error: %v", err)
 	}
 	if err := file.Close(); err != nil {
-		return fmt.Errorf("Cannot close value-dictionary file. Error: %v", err)
+		return fmt.Errorf("Cannot close dictionary file. Error: %v", err)
 	}
 	return nil
 }
 
 // Read dictionary from a binary file.
 func (d *Dictionary[K]) ReadString(filename string, magic uint64) error {
-	// clear value dictionary
+	// clear dictionary
 	d.Init()
-	// open value dictionary file for reading, read buffer, and gzip stream
+	// open dictionary file for reading, read buffer, and gzip stream
 	file, err := os.Open(filename)
 	if err != nil {
-		return fmt.Errorf("Cannot open value-dictionary file. Error: %v", err)
+		return fmt.Errorf("Cannot open dictionary file. Error: %v", err)
 	}
 	zfile, err := bzip2.NewReader(file, &bzip2.ReaderConfig{})
 	if err != nil {
-		return fmt.Errorf("Cannot open bzip2 stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot open bzip2 stream of dictionary. Error: %v", err)
 	}
 	// read and check magic number
 	var magicData uint64
 	if err := binary.Read(zfile, binary.LittleEndian, &magicData); err != nil && magicData != magic {
-		return fmt.Errorf("Cannot read magic number; value-dictionary is corrupted. Error: %v", err)
+		return fmt.Errorf("Cannot read magic number; dictionary is corrupted. Error: %v", err)
 	}
 	// read entries from file
 	for ctr := 0; true; ctr++ {
@@ -211,7 +201,7 @@ func (d *Dictionary[K]) ReadString(filename string, magic uint64) error {
 		if err == io.EOF {
 			return nil
 		} else if err != nil {
-			return fmt.Errorf("value dictionary file/reading is corrupted. Error: %v", err)
+			return fmt.Errorf("dictionary file/reading is corrupted. Error: %v", err)
 		}
 		// read byte-value
 		value := make([]byte, length)
@@ -219,19 +209,19 @@ func (d *Dictionary[K]) ReadString(filename string, magic uint64) error {
 			return fmt.Errorf("Error reading value length/file is corrupted. Error: %v", err)
 		}
 		str := string(value)
-		// envalue byte-value entry
+		// encode byte-value entry
 		idx, err := d.Encode(any(str).(K))
 		if err != nil {
-			return fmt.Errorf("Failed to envalue byte-value while reading. Error: %v", err)
+			return fmt.Errorf("Failed to encode byte-value while reading. Error: %v", err)
 		} else if idx != ctr {
-			return fmt.Errorf("Corrupted value dictionary file entries")
+			return fmt.Errorf("Corrupted dictionary file entries")
 		}
 	}
 	if err := zfile.Close(); err != nil {
-		return fmt.Errorf("Cannot close bzip2 stream of value-dictionary. Error: %v", err)
+		return fmt.Errorf("Cannot close bzip2 stream of dictionary. Error: %v", err)
 	}
 	if err := file.Close(); err != nil {
-		return fmt.Errorf("Cannot close value-dictionary file. Error: %v", err)
+		return fmt.Errorf("Cannot close dictionary file. Error: %v", err)
 	}
 	return nil
 }
