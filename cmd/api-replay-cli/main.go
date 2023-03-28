87fd4b4a91efaf57664639f3807d1607a515ed9c
Petr Hanzl <petrhanzl33@gmail.com>
2023-03-28 18:01:20 +0200

    Add SubstateDirFlag

--- cmd/api-replay-cli/main.go
+++ cmd/api-replay-cli/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Fantom-foundation/Aida/cmd/api-replay-cli/apireplay"
 	"github.com/Fantom-foundation/Aida/cmd/api-replay-cli/flags"
 	"github.com/Fantom-foundation/Aida/utils"
+	substate "github.com/Fantom-foundation/Substate"
 	"github.com/urfave/cli/v2"
 )
 
@@ -22,6 +23,7 @@ func main() {
 			&flags.WorkersFlag,
 			&flags.LogLevel,
 			&flags.Verbose,
+			&substate.SubstateDirFlag,
 			&utils.ChainIDFlag,
 			&utils.StateDbImplementationFlag,
 			&utils.StateDbVariantFlag,
