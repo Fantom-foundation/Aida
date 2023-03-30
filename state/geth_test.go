9228a8025fcfb903803b7e61a7e7411d1eb992cb
Herbert Jordan <herbert@swrnd.ch>
2023-03-30 18:17:01 +0200

    Add test code demonstrating spontanious re-creation of code

--- state/geth_test.go
+++ state/geth_test.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"bytes"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -76,4 +77,128 @@ func TestGethDbReloadData(t *testing.T) {
 	if err = db.Close(); err != nil {
 		t.Fatalf("Failed to close DB: %v", err)
 	}
+}
+
+func TestGethDb_CodeUpdateRetention(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test_db_*")
+	defer os.RemoveAll(dir)
+
+	db, err := MakeGethStateDB(dir, "", common.Hash{}, false)
+	defer func() {
+		if err = db.Close(); err != nil {
+			t.Fatalf("Failed to close DB: %v", err)
+		}
+	}()
+
+	addr1 := common.Address{1}
+	code0 := []byte{}
+	code1 := []byte{1, 2, 3}
+
+	// --- Block 1 ---
+
+	db.BeginBlock(1)
+
+	// Here the account is created and a code is assigne.d
+	db.BeginTransaction(0)
+	db.CreateAccount(addr1)
+	db.SetCode(addr1, code1)
+	db.EndTransaction()
+
+	// In this transaction it is confirmed that the code is available.
+	db.BeginTransaction(1)
+	if got := db.GetCode(addr1); !bytes.Equal(code1, got) {
+		t.Errorf("incorrect code, expected %v, got %v", code1, got)
+	}
+	db.EndTransaction()
+
+	db.EndBlock()
+
+	// --- Block 2 ---
+
+	db.BeginBlock(2)
+
+	// In this transaction we check that the code is still correct and
+	// we delete the account, after which the code should be gone (=code0)
+	db.BeginTransaction(0)
+	if got := db.GetCode(addr1); !bytes.Equal(code1, got) {
+		t.Errorf("incorrect code, expected %v, got %v", code1, got)
+	}
+	// db.Suicide(addr1)  // < when adding this, the test passes
+	db.CreateAccount(addr1)
+	if got := db.GetCode(addr1); !bytes.Equal(code0, got) {
+		t.Errorf("incorrect code, expected %v, got %v", code1, got)
+	}
+	db.EndTransaction()
+
+	// Before ending the block, we check that the code is indeed gone.
+	db.BeginTransaction(0)
+	if got := db.GetCode(addr1); !bytes.Equal(code0, got) {
+		t.Errorf("incorrect code, expected %v, got %v", code0, got)
+	}
+	db.EndTransaction()
+
+	db.EndBlock()
+
+	// --- Block 3 ---
+
+	db.BeginBlock(3)
+
+	// In the sub-sequent block, the code should be still gone.
+	db.BeginTransaction(0)
+	if got := db.GetCode(addr1); !bytes.Equal(code0, got) {
+		t.Errorf("incorrect code, expected %v, got %v", code0, got)
+	}
+	db.EndTransaction()
+	db.EndBlock()
+}
+
+func TestGethDb_NonceUpdateRetention(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test_db_*")
+	defer os.RemoveAll(dir)
+
+	db, err := MakeGethStateDB(dir, "", common.Hash{}, false)
+	defer func() {
+		if err = db.Close(); err != nil {
+			t.Fatalf("Failed to close DB: %v", err)
+		}
+	}()
+
+	addr1 := common.Address{1}
+
+	// --- Block 1 ---
+
+	db.BeginBlock(1)
+
+	db.BeginTransaction(0)
+	db.CreateAccount(addr1)
+	db.SetNonce(addr1, 12) // the nonce is 12
+	db.EndTransaction()
+
+	db.EndBlock()
+
+	// --- Block 2 ---
+
+	db.BeginBlock(2)
+	db.BeginTransaction(0)
+	if got := db.GetNonce(addr1); got != 12 {
+		t.Errorf("incorrect nonce, expected %v, got %v", 12, got)
+	}
+	db.CreateAccount(addr1) // the nonce is reverted to 0
+	if got := db.GetNonce(addr1); got != 0 {
+		t.Errorf("incorrect nonce, expected %v, got %v", 0, got)
+	}
+	db.EndTransaction()
+	db.EndBlock()
+
+	// --- Block 3 ---
+
+	db.BeginBlock(3)
+
+	db.BeginTransaction(0)
+	// Here, the nonce should be 0, as it was reported at the end of the predecessor block.
+	if got := db.GetNonce(addr1); got != 0 {
+		t.Errorf("incorrect nonce, expected %v, got %v", 0, got)
+	}
+	db.EndTransaction()
+	db.EndBlock()
 }
