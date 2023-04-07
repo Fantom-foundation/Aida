2ee8e56108d8e192dca9caa7af60d4242d78f6b4
wsodsong <wasuwee.sodsong@gmail.com>
2023-04-07 08:27:27 +0000

    Load updateset and prime
    This PR changes the priming approach. The priming procedure is
    alternating between loading update set and prime the update set to
    stateDB.

--- utils/statedb_test.go
+++ utils/statedb_test.go
@@ -153,59 +153,6 @@ func TestStatedb_InitCloseStateDB(t *testing.T) {
 	}
 }
 
-// TestStatedb_PrimeStateDB tests priming fresh state DB with randomized world state data
-func TestStatedb_PrimeStateDB(t *testing.T) {
-	for _, tc := range getStatedbTestCases() {
-		t.Run(fmt.Sprintf("DB variant: %s; shadowImpl: %s; archive variant: %s", tc.variant, tc.shadowImpl, tc.archiveVariant), func(t *testing.T) {
-			cfg := makeTestConfig(tc)
-
-			// Initialization of state DB
-			sDB, _, err := PrepareStateDB(cfg)
-
-			if err != nil {
-				t.Fatalf("failed to create state DB: %v", err)
-			}
-
-			// Closing of state DB
-			defer func(sDB state.StateDB) {
-				err = sDB.Close()
-				if err != nil {
-					t.Fatalf("failed to close state DB: %v", err)
-				}
-			}(sDB)
-
-			// Generating randomized world state
-			ws, _ := makeWorldState(t)
-
-			log := NewLogger("INFO", "TestStateDb")
-
-			// Priming state DB
-			PrimeStateDB(ws, sDB, cfg, log)
-
-			// Checks if state DB was primed correctly
-			for key, account := range ws {
-				if sDB.GetBalance(key).Cmp(account.Balance) != 0 {
-					t.Fatalf("failed to prime account balance; Is: %v; Should be: %v", sDB.GetBalance(key), account.Balance)
-				}
-
-				if sDB.GetNonce(key) != account.Nonce {
-					t.Fatalf("failed to prime account nonce; Is: %v; Should be: %v", sDB.GetNonce(key), account.Nonce)
-				}
-
-				if bytes.Compare(sDB.GetCode(key), account.Code) != 0 {
-					t.Fatalf("failed to prime account code; Is: %v; Should be: %v", sDB.GetCode(key), account.Code)
-				}
-
-				for sKey, sValue := range account.Storage {
-					if sDB.GetState(key, sKey) != sValue {
-						t.Fatalf("failed to prime account storage; Is: %v; Should be: %v", sDB.GetState(key, sKey), sValue)
-					}
-				}
-			}
-		})
-	}
-}
-
 // TestStatedb_DeleteDestroyedAccountsFromWorldState tests removal of destroyed accounts from given world state
 func TestStatedb_DeleteDestroyedAccountsFromWorldState(t *testing.T) {
 	for _, tc := range getStatedbTestCases() {
