beef8924d1432380ef4a68c379ad7ca9ae297ffc
Petr Hanzl <petrhanzl33@gmail.com>
2023-04-24 11:14:40 +0200

    Merge before PR

--- cmd/api-replay-cli/apireplay/replay_api.go
+++ cmd/api-replay-cli/apireplay/replay_api.go
@@ -7,40 +7,56 @@ import (
 
 	"github.com/Fantom-foundation/Aida/cmd/api-replay-cli/flags"
 	"github.com/Fantom-foundation/Aida/iterator"
+	"github.com/Fantom-foundation/Aida/state"
 	"github.com/Fantom-foundation/Aida/utils"
 	substate "github.com/Fantom-foundation/Substate"
 	"github.com/urfave/cli/v2"
 )
 
 func ReplayAPI(ctx *cli.Context) error {
-	iter, err := iterator.NewFileReader(context.Background(), ctx.String(flags.APIRecordingSrcFileFlag.Name))
+	var (
+		err    error
+		fr     *iterator.FileReader
+		cfg    *utils.Config
+		dbInfo utils.StateDbInfo
+		db     state.StateDB
+	)
+
+	fr, err = iterator.NewFileReader(context.Background(), ctx.String(flags.APIRecordingSrcFileFlag.Name))
 	if err != nil {
 		log.Fatalf("cannot start iter; err: %v", err)
 	}
 
-	cfg, err := utils.NewConfig(ctx, utils.BlockRangeArgs)
+	cfg, err = utils.NewConfig(ctx, utils.BlockRangeArgs)
 	if err != nil {
 		log.Fatalf("cannot create cfg; err: %v", err)
 	}
 
 	// create StateDB
-	dbInfo, err := utils.ReadStateDbInfo(filepath.Join(cfg.StateDbSrcDir, utils.DbInfoName))
+	dbInfo, err = utils.ReadStateDbInfo(filepath.Join(cfg.StateDbSrcDir, utils.DbInfoName))
 	if err != nil {
 		log.Fatalf("cannot read db info; err: %v", err)
 	}
 
-	db, err := utils.MakeStateDB(cfg.StateDbSrcDir, cfg, dbInfo.RootHash, true)
+	db, err = utils.MakeStateDB(cfg.StateDbSrcDir, cfg, dbInfo.RootHash, true)
 	if err != nil {
 		log.Fatalf("cannot make state db; err: %v", err)
 	}
 
 	substate.SetSubstateDirectory(cfg.SubstateDBDir)
 	substate.OpenSubstateDBReadOnly()
-	//defer substate.CloseSubstateDB()
 
-	r := newController(ctx, cfg, db, iter)
+	// closing gracefully both Substate and StateDB is necessary
+	defer func() {
+		err = db.Close()
+		substate.CloseSubstateDB()
+	}()
+
+	// start the replay
+	r := newController(ctx, cfg, db, fr)
 	r.Start()
 
 	r.Wait()
-	return nil
+
+	return err
 }
