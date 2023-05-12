752c344a3bd4ea015eca994d83847a47696a4f94
matejmlejnek <matej@fantom.foundation>
2023-05-12 16:38:05 +0200

    Introduce Aida-db clone command

--- cmd/db-cli/db/merge.go
+++ cmd/db-cli/db/merge.go
@@ -44,10 +44,10 @@ func merge(ctx *cli.Context) error {
 }
 
 // Merge implements merging command for combining all source data databases into single database used for profiling.
-func Merge(cfg *utils.Config, sourceDbs []string) error {
+func Merge(cfg *utils.Config, sourceDbPaths []string) error {
 	log := logger.NewLogger(cfg.LogLevel, "DB Merger")
 
-	targetDB, sourceDBs, sourceDBPaths, err := openDatabases(cfg.AidaDb, sourceDbs)
+	targetDB, sourceDBs, err := openDatabases(cfg.AidaDb, sourceDbPaths)
 	if err != nil {
 		return err
 	}
@@ -58,7 +58,7 @@ func Merge(cfg *utils.Config, sourceDbs []string) error {
 		if err != nil {
 			return err
 		}
-		log.Noticef("Merging of %s finished", sourceDBPaths[i])
+		log.Noticef("Merging of %s finished", sourceDbPaths[i])
 		// close finished sourceDB
 		MustCloseDB(sourceDB)
 	}
@@ -76,7 +76,7 @@ func Merge(cfg *utils.Config, sourceDbs []string) error {
 
 	// delete source databases
 	if cfg.DeleteSourceDbs {
-		for _, path := range sourceDBPaths {
+		for _, path := range sourceDbPaths {
 			err = os.RemoveAll(path)
 			if err != nil {
 				return err
@@ -90,41 +90,39 @@ func Merge(cfg *utils.Config, sourceDbs []string) error {
 }
 
 // openDatabases opens all databases required for merge
-func openDatabases(targetPath string, sourceDbs []string) (ethdb.Database, []ethdb.Database, []string, error) {
-	if len(sourceDbs) < 1 {
-		return nil, nil, nil, fmt.Errorf("no source database were specified\n")
+func openDatabases(targetPath string, sourceDbPaths []string) (ethdb.Database, []ethdb.Database, error) {
+	if len(sourceDbPaths) < 1 {
+		return nil, nil, fmt.Errorf("no source database were specified\n")
 	}
 
-	var sourceDBs []ethdb.Database
-	var sourceDBPaths []string
-	for i := 0; i < len(sourceDbs); i++ {
-		path := sourceDbs[i]
+	var sourceDbs []ethdb.Database
+	for i := 0; i < len(sourceDbPaths); i++ {
+		path := sourceDbPaths[i]
 		_, err := os.Stat(path)
 		if os.IsNotExist(err) {
-			return nil, nil, nil, fmt.Errorf("source database %s; doesn't exist\n", path)
+			return nil, nil, fmt.Errorf("source database %s; doesn't exist\n", path)
 		}
 		db, err := rawdb.NewLevelDBDatabase(path, 1024, 100, "", true)
 		if err != nil {
-			return nil, nil, nil, fmt.Errorf("source database %s; error: %v", path, err)
+			return nil, nil, fmt.Errorf("source database %s; error: %v", path, err)
 		}
-		sourceDBPaths = append(sourceDBPaths, path)
-		sourceDBs = append(sourceDBs, db)
+		sourceDbs = append(sourceDbs, db)
 	}
 
-	// open targetDB
-	targetDB, err := rawdb.NewLevelDBDatabase(targetPath, 1024, 100, "profiling", false)
+	// open targetDb
+	targetDb, err := rawdb.NewLevelDBDatabase(targetPath, 1024, 100, "profiling", false)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("targetDB. Error: %v", err)
+		return nil, nil, fmt.Errorf("targetDb. Error: %v", err)
 	}
 
-	return targetDB, sourceDBs, sourceDBPaths, nil
+	return targetDb, sourceDbs, nil
 }
 
-// copyData copies data from source to target database, substitute
-func copyData(sourceDB ethdb.Database, targetDB ethdb.Database) error {
-	dbBatchWriter := targetDB.NewBatch()
+// copyData copies data from iterator into target database
+func copyData(sourceDb ethdb.Database, targetDb ethdb.Database) error {
+	dbBatchWriter := targetDb.NewBatch()
 
-	iter := sourceDB.NewIterator(nil, nil)
+	iter := sourceDb.NewIterator(nil, nil)
 	for {
 		// do we have another available item?
 		if !iter.Next() {
