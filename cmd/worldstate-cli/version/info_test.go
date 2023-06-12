0c45afc37e78172f0f5cf42eeaa6e6eea145ce67
Andrei Ivasko <andrei@fantom.foundation>
2023-06-12 13:16:46 +0200

    goimports

--- cmd/worldstate-cli/version/info_test.go
+++ cmd/worldstate-cli/version/info_test.go
@@ -11,8 +11,8 @@ func TestVersionInfoGivesOutput(t *testing.T) {
 		"App Version": regexp.MustCompile(`(?m)^\[\d+(;\d+)?mApp\sVersion:\[\d+(;\d+)?m\s+[\d.]+$`),
 		"Commit Hash": regexp.MustCompile(`(?m)^\[\d+(;\d+)?mCommit\sHash:\[\d+(;\d+)?m\s+[a-f\d.]{40}$`),
 		"Commit Time": regexp.MustCompile(`(?m)^\[\d+(;\d+)?mCommit\sTime:\[\d+(;\d+)?m\s+\w{3}, [0-9]{2} \w{3} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}(\s-?[0-9]{4})?$`),
-		"Build Time": regexp.MustCompile(`(?m)^\[\d+(;\d+)?mBuild\sTime:\[\d+(;\d+)?m\s+\w{3}, [0-9]{2} \w{3} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}(\s-?[0-9]{4})?$`),
-		"Compiler": regexp.MustCompile(`(?m)^\[\d+(;\d+)?mCompiler:\[\d+(;\d+)?m\s+\w+`),
+		"Build Time":  regexp.MustCompile(`(?m)^\[\d+(;\d+)?mBuild\sTime:\[\d+(;\d+)?m\s+\w{3}, [0-9]{2} \w{3} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}(\s-?[0-9]{4})?$`),
+		"Compiler":    regexp.MustCompile(`(?m)^\[\d+(;\d+)?mCompiler:\[\d+(;\d+)?m\s+\w+`),
 	}
 
 	v := Long()
