709dc6f26424a1269e4f963dc0398579b41df4d7
Lubomír Jahn <lubomir.j@icloud.com>
2023-07-03 14:49:43 +0200

    Fix default values of non-specified flags in config

---
+++ utils/default_config.go
@@ -0,0 +1,138 @@
+package utils
+
+import (
+	"github.com/Fantom-foundation/Aida/cmd/db-cli/flags"
+	"github.com/Fantom-foundation/Aida/logger"
+	substate "github.com/Fantom-foundation/Substate"
+	"github.com/urfave/cli/v2"
+)
+
+// createConfig returns Config instance with user specified values or the default ones
+func createConfig(ctx *cli.Context) *Config {
+	cfg := &Config{
+		AppName:     ctx.App.HelpName,
+		CommandName: ctx.Command.Name,
+
+		APIRecordingSrcFile: getFlagValue(ctx, APIRecordingSrcFileFlag).(string),
+		ArchiveMode:         getFlagValue(ctx, ArchiveModeFlag).(bool),
+		ArchiveVariant:      getFlagValue(ctx, ArchiveVariantFlag).(string),
+		BlockLength:         getFlagValue(ctx, BlockLengthFlag).(uint64),
+		BalanceRange:        getFlagValue(ctx, BalanceRangeFlag).(int64),
+		CarmenSchema:        getFlagValue(ctx, CarmenSchemaFlag).(int),
+		ChainID:             getFlagValue(ctx, ChainIDFlag).(int),
+		Cache:               getFlagValue(ctx, CacheFlag).(int),
+		ContractNumber:      getFlagValue(ctx, ContractNumberFlag).(int64),
+		ContinueOnFailure:   getFlagValue(ctx, ContinueOnFailureFlag).(bool),
+		CPUProfile:          getFlagValue(ctx, CpuProfileFlag).(string),
+		Db:                  getFlagValue(ctx, DbFlag).(string),
+		DbTmp:               getFlagValue(ctx, DbTmpFlag).(string),
+		Debug:               getFlagValue(ctx, TraceDebugFlag).(bool),
+		DebugFrom:           getFlagValue(ctx, DebugFromFlag).(uint64),
+		Quiet:               getFlagValue(ctx, QuietFlag).(bool),
+		SyncPeriodLength:    getFlagValue(ctx, SyncPeriodLengthFlag).(uint64),
+		Genesis:             getFlagValue(ctx, GenesisFlag).(string),
+		DbImpl:              getFlagValue(ctx, StateDbImplementationFlag).(string),
+		DbVariant:           getFlagValue(ctx, StateDbVariantFlag).(string),
+		DbLogging:           getFlagValue(ctx, StateDbLoggingFlag).(bool),
+		DeletionDb:          getFlagValue(ctx, DeletionDbFlag).(string),
+		DeleteSourceDbs:     getFlagValue(ctx, DeleteSourceDbsFlag).(bool),
+		CompactDb:           getFlagValue(ctx, CompactDbFlag).(bool),
+		HasDeletedAccounts:  true,
+		KeepDb:              getFlagValue(ctx, KeepDbFlag).(bool),
+		KeysNumber:          getFlagValue(ctx, KeysNumberFlag).(int64),
+		MaxNumTransactions:  getFlagValue(ctx, MaxNumTransactionsFlag).(int),
+		MemoryBreakdown:     getFlagValue(ctx, MemoryBreakdownFlag).(bool),
+		MemoryProfile:       getFlagValue(ctx, MemoryProfileFlag).(string),
+		NonceRange:          getFlagValue(ctx, NonceRangeFlag).(int),
+		TransactionLength:   getFlagValue(ctx, TransactionLengthFlag).(uint64),
+		PrimeRandom:         getFlagValue(ctx, RandomizePrimingFlag).(bool),
+		RandomSeed:          getFlagValue(ctx, RandomSeedFlag).(int64),
+		PrimeThreshold:      getFlagValue(ctx, PrimeThresholdFlag).(int),
+		Profile:             getFlagValue(ctx, ProfileFlag).(bool),
+		ProfileFile:         getFlagValue(ctx, ProfileFileFlag).(string),
+		ProfileInterval:     getFlagValue(ctx, ProfileIntervalFlag).(uint64),
+		SkipPriming:         getFlagValue(ctx, SkipPrimingFlag).(bool),
+		SkipMetadata:        getFlagValue(ctx, flags.SkipMetadata).(bool),
+		ShadowDb:            getFlagValue(ctx, ShadowDb).(bool),
+		ShadowImpl:          getFlagValue(ctx, ShadowDbImplementationFlag).(string),
+		ShadowVariant:       getFlagValue(ctx, ShadowDbVariantFlag).(string),
+		SnapshotDepth:       getFlagValue(ctx, SnapshotDepthFlag).(int),
+		StateDbSrc:          getFlagValue(ctx, StateDbSrcFlag).(string),
+		AidaDb:              getFlagValue(ctx, AidaDbFlag).(string),
+		Output:              getFlagValue(ctx, OutputFlag).(string),
+		StateValidationMode: EqualityCheck,
+		UpdateDb:            getFlagValue(ctx, UpdateDbFlag).(string),
+		SubstateDb:          getFlagValue(ctx, substate.SubstateDbFlag).(string),
+		OperaDatadir:        getFlagValue(ctx, OperaDatadirFlag).(string),
+		ValuesNumber:        getFlagValue(ctx, ValuesNumberFlag).(int64),
+		Validate:            getFlagValue(ctx, ValidateFlag).(bool),
+		VmImpl:              getFlagValue(ctx, VmImplementation).(string),
+		Workers:             getFlagValue(ctx, substate.WorkersFlag).(int),
+		WorldStateDb:        getFlagValue(ctx, WorldStateFlag).(string),
+		TraceFile:           getFlagValue(ctx, TraceFileFlag).(string),
+		Trace:               getFlagValue(ctx, TraceFlag).(bool),
+		LogLevel:            getFlagValue(ctx, logger.LogLevelFlag).(string),
+		SourceTableName:     getFlagValue(ctx, SourceTableNameFlag).(string),
+		TargetDb:            getFlagValue(ctx, TargetDbFlag).(string),
+		TrieRootHash:        getFlagValue(ctx, TrieRootHashFlag).(string),
+		IncludeStorage:      getFlagValue(ctx, IncludeStorageFlag).(bool),
+		ProfileEVMCall:      getFlagValue(ctx, ProfileEVMCallFlag).(bool),
+		MicroProfiling:      getFlagValue(ctx, MicroProfilingFlag).(bool),
+		BasicBlockProfiling: getFlagValue(ctx, BasicBlockProfilingFlag).(bool),
+		OnlySuccessful:      getFlagValue(ctx, OnlySuccessfulFlag).(bool),
+		ProfilingDbName:     getFlagValue(ctx, ProfilingDbNameFlag).(string),
+		ChannelBufferSize:   getFlagValue(ctx, ChannelBufferSizeFlag).(int),
+		TargetBlock:         getFlagValue(ctx, TargetBlockFlag).(uint64),
+		UpdateBufferSize:    getFlagValue(ctx, UpdateBufferSizeFlag).(uint64),
+	}
+
+	return cfg
+}
+
+// getFlagValue returns value specified by user if flag is present in cli context, otherwise return default flag value
+func getFlagValue(ctx *cli.Context, flag interface{}) interface{} {
+	cmdFlags := ctx.Command.Flags
+	for _, cmdFlag := range cmdFlags {
+		switch f := flag.(type) {
+		case cli.IntFlag:
+			if cmdFlag.Names()[0] == f.Name {
+				return ctx.Int(f.Name)
+			}
+			return f.Value
+
+		case cli.Uint64Flag:
+			if cmdFlag.Names()[0] == UpdateBufferSizeFlag.Name {
+				return ctx.Uint64(f.Name) * 1_000_000
+			} else if cmdFlag.Names()[0] == f.Name {
+				return ctx.Uint64(f.Name)
+			}
+			return f.Value
+
+		case cli.Int64Flag:
+			if cmdFlag.Names()[0] == f.Name {
+				return ctx.Int64(f.Name)
+			}
+			return f.Value
+
+		case cli.StringFlag:
+			if cmdFlag.Names()[0] == f.Name {
+				return ctx.String(f.Name)
+			}
+			return f.Value
+
+		case cli.PathFlag:
+			if cmdFlag.Names()[0] == f.Name {
+				return ctx.Path(f.Name)
+			}
+			return f.Value
+
+		case cli.BoolFlag:
+			if cmdFlag.Names()[0] == f.Name {
+				return ctx.Bool(f.Name)
+			}
+			return f.Value
+		}
+	}
+
+	return nil
+}
