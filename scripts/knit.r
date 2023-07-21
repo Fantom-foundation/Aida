8fae2f4895add92c1a5697dc7a8a97add1b31337
Andrei Ivasko <andrei@fantom.foundation>
2023-07-21 14:21:13 +0200

    knit.r

--- scripts/knit.r
+++ scripts/knit.r
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 library(rmarkdown)
+args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
