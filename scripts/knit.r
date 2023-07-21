738e3c8b43cb40ffbbbe379ce68419a29c5c780a
Andrei Ivasko <andrei@fantom.foundation>
2023-07-21 14:04:00 +0200

    update scripts/knit.r

--- scripts/knit.r
+++ scripts/knit.r
@@ -1,7 +1,10 @@
 #!/usr/bin/env Rscript
 library(rmarkdown)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
 render(
   input = './reports/parallel_experiment.rmd', 
   params = list(
-    ProfileDB = './profile.db'
+    ProfileDB = args[1]
 ))
