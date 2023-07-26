e0edf6274b5d6e158252a6f0513967b2e1039440
Andrei Ivasko <andrei@fantom.foundation>
2023-07-26 18:44:41 +0300

    add some prints to knir.R

--- scripts/knit.R
+++ scripts/knit.R
@@ -16,6 +16,7 @@ library(optparse)
 library(tools)
 
 # parse command-line arguments 
+cat("Parse command line arguments...\n")
 option_list <- list(
     make_option(c("-d", "--db"),         default="./data.db",       help="db for rendering experiment."),
     make_option(c("-o", "--output"),     default="./report",        help="output file"),
@@ -26,6 +27,7 @@ option_list <- list(
 opt <- parse_args(OptionParser(usage="%prog [options] file", option_list=option_list), positional_argument=1)
 
 # retrieve options and argument
+cat("Retrieve options and argument\n")
 db <- opt$options$db
 output <- opt$options$output
 outputDirectory <- opt$options$`output-dir`
@@ -35,6 +37,7 @@ parameter <- c(list( db = normalizePath(db)), parameter)
 rmdFile <- opt$args 
 
 # check output format
+cat("Check output format\n")
 if (outputFormat == "html") {
     docFormat = "html_document"
 } else if (outputFormat == "pdf") {
@@ -46,21 +49,25 @@ if (outputFormat == "html") {
 }
 
 # check whether R Markdown file exists
+cat("check whether R Markdown file exists\n")
 if(file.access(rmdFile)==-1) {
     stop(sprintf("R-Markdown file %s cannot be found", rmdFile))
 }
 
 # check whether the profile DB file exists
+cat("check whether the profile DB file exists\n")
 if(file.access(db)==-1) {
     stop(sprintf("Profile DB %s cannot be found", db))
 }
 
 # check whether output directory exists
+cat("check whether output directory exists\n")
 if(file.access(outputDirectory)==-1) {
     stop(sprintf("Output directory %s cannot be found", outputDirectory))
 }
 
 # render the R markdown document
+cat("render the R markdown document\n")
 render(
     input = rmdFile, 
     output_file = output, 
