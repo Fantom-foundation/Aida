4b8ae2fc60e2bc60af445029c7d041871ea8322e
Andrei Ivasko <andrei@fantom.foundation>
2023-08-02 16:25:58 +0300

    add profiledb tests

--- profile/parallelisation/profiledb_test.go
+++ profile/parallelisation/profiledb_test.go
@@ -3,8 +3,8 @@ package parallelisation
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
+	"os"
 	"testing"
 	"time"
 
@@ -13,7 +13,7 @@ import (
 )
 
 func tempFile(require *require.Assertions) string {
-	file, err := ioutil.TempFile("", "*.db")
+	file, err := os.CreateTemp("", "*.db")
 	require.NoError(err)
 	file.Close()
 	return file.Name()
@@ -29,20 +29,116 @@ func TestAdd(t *testing.T) {
 	defer db.Close()
 
 	ProfileData := ProfileData{
-		curBlock:    5637800,
-		tBlock:      5838,
-		tSequential: 4439,
-		tCritical:   2424,
-		tCommit:     1398,
-		speedup:     1.527263,
-		ubNumProc:   2,
-		numTx:       3,
+		curBlock:      5637800,
+		tBlock:        5838,
+		tSequential:   4439,
+		tCritical:     2424,
+		tCommit:       1398,
+		speedup:       1.527263,
+		ubNumProc:     2,
+		numTx:         3,
+		tTransactions: []int64{2382388, 11218838, 5939392888},
 	}
 
 	err = db.Add(ProfileData)
 	require.NoError(err)
 
 	require.Equal(len(db.buffer), 1)
+
+	require.Equal(len(db.buffer[0].tTransactions), 3)
+}
+
+func TestFlush(t *testing.T) {
+	// db has 0 records
+	require := require.New(t)
+	dbFile := tempFile(require)
+	t.Logf("db file: %s", dbFile)
+	db, err := NewProfileDB(dbFile)
+	require.NoError(err)
+	err = db.Add(ProfileData{})
+	require.NoError(err)
+
+	err = db.Flush()
+	require.NoError(err)
+	db.Close()
+
+	// db has 2 records
+	db, err = NewProfileDB(dbFile)
+	require.NoError(err)
+
+	pd := ProfileData{
+		curBlock:      5637800,
+		tBlock:        5838,
+		tSequential:   4439,
+		tCritical:     2424,
+		tCommit:       1398,
+		speedup:       1.527263,
+		ubNumProc:     2,
+		numTx:         3,
+		tTransactions: []int64{2382388, 11218838, 5939392888},
+	}
+
+	err = db.Add(pd)
+	require.NoError(err)
+
+	pd = ProfileData{
+		curBlock:      3239933,
+		tBlock:        44939,
+		tSequential:   3493848,
+		tCritical:     434838,
+		tCommit:       2332,
+		speedup:       1.203983,
+		ubNumProc:     2,
+		numTx:         2,
+		tTransactions: []int64{2382388, 11218838},
+	}
+	err = db.Add(pd)
+	require.NoError(err)
+	require.Len(db.buffer, 2)
+	require.Len(db.buffer[0].tTransactions, 3)
+	require.Len(db.buffer[1].tTransactions, 2)
+	err = db.Flush()
+	require.NoError(err)
+	require.Len(db.buffer, 0)
+	db.Close()
+
+	// trigger Flush method inside Add
+	db, err = NewProfileDB(dbFile)
+	require.NoError(err)
+	defer db.Close()
+
+	for i := 1; i < bufferSize; i++ {
+		profileData := ProfileData{
+			curBlock:      uint64(i),
+			tBlock:        5838,
+			tSequential:   4439,
+			tCritical:     2424,
+			tCommit:       1398,
+			speedup:       1.527263,
+			ubNumProc:     2,
+			numTx:         2,
+			tTransactions: []int64{2382388, 11218838},
+		}
+		err = db.Add(profileData)
+		require.NoError(err)
+		require.Len(db.buffer, i)
+	}
+
+	pd = ProfileData{
+		curBlock:      uint64(bufferSize),
+		tBlock:        5838,
+		tSequential:   4439,
+		tCritical:     2424,
+		tCommit:       1398,
+		speedup:       1.527263,
+		ubNumProc:     2,
+		numTx:         3,
+		tTransactions: []int64{2382388, 11218838, 232348228},
+	}
+
+	err = db.Add(pd)
+	require.NoError(err)
+	require.Len(db.buffer, 0)
 }
 
 // TestDeleteBlockRangeOverlap tests profileDB.DeleteByBlockRange function
@@ -175,14 +271,15 @@ func ExampleDB() {
 	const count = 10_000
 	for i := 0; i < count; i++ {
 		ProfileData := ProfileData{
-			curBlock:    5637800,
-			tBlock:      5838,
-			tSequential: 4439,
-			tCritical:   2424,
-			tCommit:     1398,
-			speedup:     rand.Float64() * 10,
-			ubNumProc:   2,
-			numTx:       3,
+			curBlock:      5637800,
+			tBlock:        5838,
+			tSequential:   4439,
+			tCritical:     2424,
+			tCommit:       1398,
+			speedup:       rand.Float64() * 10,
+			ubNumProc:     2,
+			numTx:         3,
+			tTransactions: []int64{2382388, 11218838, 5939392888},
 		}
 		if err := db.Add(ProfileData); err != nil {
 			fmt.Println("ERROR: insert - ", err)
@@ -193,4 +290,49 @@ func ExampleDB() {
 	fmt.Printf("inserted %d records\n", count)
 	// Output:
 	// inserted 10000 records
+}
+
+func TestFlushProfileData(t *testing.T) {
+	require := require.New(t)
+	dbFile := tempFile(require)
+	t.Logf("db file: %s", dbFile)
+
+	db, err := NewProfileDB(dbFile)
+	require.NoError(err)
+	defer db.Close()
+
+	ProfileData := ProfileData{
+		curBlock:      5637800,
+		tBlock:        5838,
+		tSequential:   4439,
+		tCritical:     2424,
+		tCommit:       1398,
+		speedup:       1.527263,
+		ubNumProc:     2,
+		numTx:         3,
+		tTransactions: []int64{292988, 8387773, 923828772},
+	}
+
+	// start db transaction
+	tx, err := db.sql.Begin()
+	require.NoError(err)
+	res, err := tx.Stmt(db.blockStmt).Exec(ProfileData.curBlock, ProfileData.tBlock, ProfileData.tSequential, ProfileData.tCritical,
+		ProfileData.tCommit, ProfileData.speedup, ProfileData.ubNumProc, ProfileData.numTx)
+	require.NoError(err)
+	numRowsAffected, err := res.RowsAffected()
+	require.NoError(err)
+	if numRowsAffected != 1 {
+		t.Errorf("invalid numRowsAffected value")
+	}
+
+	for i, tTransaction := range ProfileData.tTransactions {
+		res, err = tx.Stmt(db.txStmt).Exec(ProfileData.curBlock, i, tTransaction)
+		require.NoError(err)
+		numRowsAffected, err := res.RowsAffected()
+		require.NoError(err)
+		if numRowsAffected != 1 {
+			t.Errorf("invalid numRowsAffected value")
+		}
+	}
+	require.NoError(tx.Commit())
 }
