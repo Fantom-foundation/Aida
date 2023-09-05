841107ebe0d8eacc8ad8083083e0ac45455936b2
Petr Hanzl <84449820+petr-hanzl@users.noreply.github.com>
2023-09-05 16:38:29 +0200

    Progress logger (#646)
    * Add progress logger as an extension to new processor.

---
+++ executor/extension/progress_logger.go
@@ -0,0 +1,117 @@
+package extension
+
+import (
+	"math/big"
+	"sync"
+	"time"
+
+	"github.com/Fantom-foundation/Aida/executor"
+	"github.com/Fantom-foundation/Aida/logger"
+	"github.com/Fantom-foundation/Aida/utils"
+)
+
+const (
+	DefaultReportFrequencyInBlocks = 100_000
+	progressReportFormat           = "Elapsed time: %v; reached block %d; last interval rate ~%.2f Tx/s"
+)
+
+// MakeProgressLogger creates progress logger. It logs progress about processor depending on reportFrequency.
+// If reportFrequency is 0, it is set to DefaultReportFrequencyInBlocks.
+func MakeProgressLogger(config *utils.Config, reportFrequency int) executor.Extension {
+	if config.Quiet {
+		return NilExtension{}
+	}
+
+	if reportFrequency == 0 {
+		reportFrequency = DefaultReportFrequencyInBlocks
+	}
+
+	return &progressLogger{
+		config:          config,
+		log:             logger.NewLogger(config.LogLevel, "Progress-Reporter"),
+		inputCh:         make(chan executor.State, 10),
+		wg:              new(sync.WaitGroup),
+		reportFrequency: reportFrequency,
+	}
+}
+
+type progressLogger struct {
+	NilExtension
+	config          *utils.Config
+	log             logger.Logger
+	inputCh         chan executor.State
+	wg              *sync.WaitGroup
+	reportFrequency int
+}
+
+// PreRun starts the report goroutine
+func (l *progressLogger) PreRun(_ executor.State) error {
+	l.wg.Add(1)
+
+	// pass the value for thread safety
+	go l.startReport(l.reportFrequency)
+	return nil
+}
+
+// PostRun gracefully closes the Extension and awaits the report goroutine correct closure.
+func (l *progressLogger) PostRun(_ executor.State, _ error) error {
+	close(l.inputCh)
+	l.wg.Wait()
+
+	return nil
+}
+
+// PostBlock sends the state to the report goroutine.
+// We only care about total number of transactions we can do this here rather in PostTransaction.
+func (l *progressLogger) PostBlock(state executor.State) error {
+	l.inputCh <- state
+	return nil
+}
+
+// startReport runs in own goroutine. It accepts data from Executor from PostBock func.
+// It reports current progress everytime we hit the ticker with defaultReportFrequencyInSeconds.
+func (l *progressLogger) startReport(reportFrequency int) {
+	start := time.Now()
+	lastReport := time.Now()
+
+	totalTx := new(big.Int)
+	totalBlocks := new(big.Int)
+	var lastIntervalTx uint64
+
+	defer func() {
+		elapsed := time.Since(start)
+		txRate := float64(totalTx.Uint64()) / elapsed.Seconds()
+
+		l.log.Infof(progressReportFormat, elapsed.Round(time.Second), totalBlocks.Uint64(), txRate)
+
+		l.wg.Done()
+	}()
+
+	var (
+		in executor.State
+		ok bool
+	)
+	for {
+		select {
+		case in, ok = <-l.inputCh:
+			if !ok {
+				return
+			}
+
+			// we must do tx + 1 because first tx is actually marked as 0
+			lastIntervalTx += uint64(in.Transaction + 1)
+			totalBlocks.SetUint64(uint64(in.Block))
+
+			// did we hit the block milestone?
+			if in.Block%reportFrequency == 0 {
+				elapsed := time.Since(start)
+				txRate := float64(lastIntervalTx) / time.Since(lastReport).Seconds()
+
+				// todo add file size and gas rate once StateDb is added to new processor
+				l.log.Infof(progressReportFormat, elapsed.Round(1*time.Second), totalBlocks.Uint64(), txRate)
+				lastReport = time.Now()
+			}
+		}
+	}
+
+}
---
+++ executor/extension/progress_logger_test.go
@@ -0,0 +1,120 @@
+package extension
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Fantom-foundation/Aida/executor"
+	"github.com/Fantom-foundation/Aida/logger"
+	"github.com/Fantom-foundation/Aida/utils"
+	"go.uber.org/mock/gomock"
+)
+
+const testReportFrequency = 1
+
+func TestProgressLoggerExtension_CorrectClose(t *testing.T) {
+	config := &utils.Config{}
+	ext := MakeProgressLogger(config, testReportFrequency)
+
+	// start the report thread
+	ext.PreRun(executor.State{})
+
+	// make sure PostRun is not blocking.
+	done := make(chan bool)
+	go func() {
+		ext.PostRun(executor.State{}, nil)
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return
+	case <-time.After(time.Second):
+		t.Fatalf("PostRun blocked unexpectedly")
+	}
+}
+
+func TestProgressLoggerExtension_NoLoggerIsCreatedIfDisabled(t *testing.T) {
+	config := &utils.Config{}
+	config.Quiet = true
+	ext := MakeProgressLogger(config, testReportFrequency)
+	if _, ok := ext.(NilExtension); !ok {
+		t.Errorf("Logger is enabled although not set in configuration")
+	}
+
+}
+
+func TestProgressLoggerExtension_LoggingHappens(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	log := logger.NewMockLogger(ctrl)
+
+	config := &utils.Config{}
+	config.Quiet = true
+
+	ext := progressLogger{
+		config:          config,
+		log:             log,
+		inputCh:         make(chan executor.State, 10),
+		wg:              new(sync.WaitGroup),
+		reportFrequency: testReportFrequency,
+	}
+
+	ext.PreRun(executor.State{})
+
+	gomock.InOrder(
+		log.EXPECT().Infof(MatchFormat(progressReportFormat), gomock.Any(), uint64(1), MatchTxRate()),
+		log.EXPECT().Infof(MatchFormat(progressReportFormat), gomock.Any(), uint64(2), MatchTxRate()),
+	)
+
+	// fill the logger with some data
+	ext.PostBlock(executor.State{
+		Block:       1,
+		Transaction: 1,
+	})
+
+	ext.PostBlock(executor.State{
+		Block:       2,
+		Transaction: 2,
+	})
+
+	// wait a bit until the logger gets the data
+	time.Sleep(time.Second)
+}
+
+// MATCHERS
+
+func MatchFormat(format string) gomock.Matcher {
+	return matchFormat{format}
+}
+
+type matchFormat struct {
+	format string
+}
+
+func (m matchFormat) Matches(value any) bool {
+	format, ok := value.(string)
+	return ok && strings.Compare(m.format, format) == 0
+}
+
+func (m matchFormat) String() string {
+	return fmt.Sprintf("log format should look like this: %v", m.format)
+}
+
+func MatchTxRate() gomock.Matcher {
+	return matchTxRate{}
+}
+
+type matchTxRate struct {
+}
+
+func (m matchTxRate) Matches(value any) bool {
+	txRate, ok := value.(float64)
+	return ok && txRate > 0
+}
+
+func (m matchTxRate) String() string {
+	return fmt.Sprintf("log should have a txRate that is larger than 0")
+}
