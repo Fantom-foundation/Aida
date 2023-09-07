8309b2162e83e2b5f2651b188364ba0ad4566e3b
Petr Hanzl <petrhanzl33@gmail.com>
2023-09-07 15:54:08 +0200

    Gather all errors into a list

--- executor/extension/tx_validator.go
+++ executor/extension/tx_validator.go
@@ -3,6 +3,7 @@ package extension
 import (
 	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/Fantom-foundation/Aida/executor"
 	"github.com/Fantom-foundation/Aida/logger"
@@ -17,7 +18,9 @@ type txValidator struct {
 	log             logger.Logger
 	updateOnFailure bool
 	maxErrors       int
-	errCounter      int
+	lock            sync.Mutex
+	inputErrors     []error
+	outputErrors    []error
 }
 
 func MakeTxValidator(config *utils.Config, updateOnFailure bool, maxErrors int) executor.Extension {
@@ -39,50 +42,93 @@ func MakeTxValidator(config *utils.Config, updateOnFailure bool, maxErrors int) 
 
 // PreRun informs user the txValidator is enabled thus he should count with slower processing speed.
 func (v *txValidator) PreRun(_ executor.State) error {
-	v.log.Warning("Transaction verification is enabled, this will drastically slow down the process.")
+
+	v.log.Warning("Transaction verification is enabled, this may slow down the process.")
 
 	if v.config.ContinueOnFailure {
 		v.log.Warningf("Continue on Failure for tx validation is enabled though "+
-			"program will exit when %v errors occur!", v.errCounter)
+			"program will exit when %v errors occur!", v.maxErrors)
 	}
 
 	return nil
 }
 
-// PostTransaction validates StateDB. If any error occurs and ContinueOnFailure is enabled, if
-// adds one error to the counter (txValidator.errCounter). If the counter is larger than threshold
-// (txValidator.maxErrors), or ContinueOnFailure is disabled, error is returned exiting the program.
+// PreTransaction validates InputAlloc in given substate
+func (v *txValidator) PreTransaction(state executor.State) error {
+	err := utils.ValidateStateDB(state.Substate.InputAlloc, state.State, v.updateOnFailure)
+	if err == nil {
+		return nil
+	}
+
+	// this func must be thread safe
+	v.lock.Lock()
+	defer v.lock.Unlock()
+
+	e := fmt.Errorf("input error at block %v tx %v; %v", state.Block, state.Transaction, err)
+	// ContinueOnFailure is disabled, return the error thus exit the program
+	if !v.config.ContinueOnFailure {
+		return e
+	}
+
+	v.log.Error(e)
+	// ContinueOnFailure is enabled, log the error to user
+	v.inputErrors = append(v.inputErrors, err)
+
+	// check if error cap has been reached
+	if len(v.inputErrors)+len(v.outputErrors) >= v.maxErrors {
+		return errors.New("maximum number of errors occurred")
+	}
+
+	return nil
+}
+
+// PostTransaction validates OutputAlloc in given substate
 func (v *txValidator) PostTransaction(state executor.State) error {
 	err := utils.ValidateStateDB(state.Substate.InputAlloc, state.State, v.updateOnFailure)
 	if err == nil {
 		return nil
 	}
 
-	v.errCounter++
+	// this func must be thread safe
+	v.lock.Lock()
+	defer v.lock.Unlock()
 
+	e := fmt.Errorf("output error at block %v tx %v; %v", state.Block, state.Transaction, err)
+	// ContinueOnFailure is disabled, return the error thus exit the program
 	if !v.config.ContinueOnFailure {
-		return fmt.Errorf("input alloc is not contained in the state_db; %v", err)
+		return e
 	}
 
-	v.log.Errorf("input alloc is not contained in the state_db; %v", err)
+	v.log.Error(e)
+	// ContinueOnFailure is enabled, log the error to user
+	v.inputErrors = append(v.inputErrors, err)
 
-	if v.errCounter >= v.maxErrors {
+	// check if error cap has been reached
+	if len(v.inputErrors)+len(v.outputErrors) >= v.maxErrors {
 		return errors.New("maximum number of errors occurred")
 	}
-
-	v.log.Warningf("Error number %v", v.errCounter)
 
 	return nil
 }
 
 // PostRun informs user how many errors were found - if ContinueOnFailureIsEnabled otherwise success is reported.
 func (v *txValidator) PostRun(_ executor.State, _ error) error {
-	if v.config.ContinueOnFailure {
-		v.log.Noticef("Validation finished! In total %v errors occurred", v.errCounter)
-	} else {
+	v.lock.Lock()
+	defer v.lock.Unlock()
+
+	// no errors occurred
+	if len(v.outputErrors) == 0 && len(v.inputErrors) == 0 {
 		v.log.Noticef("Validation successful!")
+		return nil
 	}
 
-	return nil
+	v.log.Warningf("%v output errors caught", len(v.outputErrors))
+	v.log.Warningf("%v input errors caught", len(v.inputErrors))
 
+	var err error
+
+	err = errors.Join(v.inputErrors...)
+	err = errors.Join(v.outputErrors...)
+
+	return err
 }
