7e9ed5989e2661edb068912170bc8ff882d52113
Herbert Jordan <herbert@swrnd.ch>
2023-10-04 17:14:20 +0200

    Update executor/extension/diagnostic.go
    Co-authored-by: Petr Hanzl <84449820+petr-hanzl@users.noreply.github.com>

--- executor/extension/diagnostic.go
+++ executor/extension/diagnostic.go
@@ -38,7 +38,8 @@ func (e *diagnosticServer) PreRun(executor.State, *executor.Context) error {
 	e.log.Infof("Starting diagnostic server at port http://localhost:%d (see https://pkg.go.dev/net/http/pprof#hdr-Usage_examples for usage examples)", e.port)
 	e.log.Warning("Block and mutex sampling rate is set to 100%% for diagnostics, which may impact overall performance")
 	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
+		addr := fmt.Sprintf("localhost:%d", e.port)
+		log.Println(http.ListenAndServe(addr, nil))
 	}()
 	runtime.SetBlockProfileRate(1)
 	runtime.SetMutexProfileFraction(1)
