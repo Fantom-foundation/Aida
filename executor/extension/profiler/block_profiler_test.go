3ea4425c7d2ea95b7877b900491a3d7495194866
Petr Hanzl <petrhanzl33@gmail.com>
2023-10-16 13:40:40 +0200

    Add test for db creation

--- executor/extension/profiler/block_profiler_test.go
+++ executor/extension/profiler/block_profiler_test.go
@@ -1,8 +1,11 @@
 package profiler
 
 import (
+	"errors"
+	"os"
 	"testing"
 
+	"github.com/Fantom-foundation/Aida/executor"
 	"github.com/Fantom-foundation/Aida/executor/extension"
 	"github.com/Fantom-foundation/Aida/utils"
 )
@@ -13,5 +16,25 @@ func TestBlockProfilerExtension_NoProfileIsCollectedIfDisabled(t *testing.T) {
 
 	if _, ok := ext.(extension.NilExtension); !ok {
 		t.Errorf("profiler is enabled although not set in configuration")
+	}
+}
+
+func TestBlockProfilerExtension_ProfileDbIsCreated(t *testing.T) {
+	path := t.TempDir() + "/profile.db"
+	config := &utils.Config{}
+	config.ProfileBlocks = true
+	config.ProfileDB = path
+
+	ext := MakeBlockProfiler(config)
+
+	if err := ext.PreRun(executor.State{}, nil); err != nil {
+		t.Fatalf("unexpected error during pre-run; %v", err)
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			t.Fatal("db was not created")
+		}
+		t.Fatalf("unexpected error; %v", err)
 	}
 }
