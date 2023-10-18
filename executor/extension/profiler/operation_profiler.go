dc3ae98e5b5ec369c3c425f00cfab88ede01ddb0
rapol-fantom <rapol@fantom.foundation>
2023-10-18 14:10:30 +0000

    remove unnecssary whitespace

--- executor/extension/profiler/operation_profiler.go
+++ executor/extension/profiler/operation_profiler.go
@@ -46,6 +46,7 @@ func (p *operationProfiler[T]) PreBlock(state executor.State[T], _ *executor.Con
 
 	return nil
 }
+<<<<<<< HEAD
 
 func (p *operationProfiler[T]) PostBlock(state executor.State[T], _ *executor.Context) error {
 	p.lastProcessedBlock = uint64(state.Block)
@@ -56,3 +57,5 @@ func (p *operationProfiler[T]) PostRun(executor.State[T], *executor.Context, err
 	p.stats.PrintProfiling(p.interval.Start(), p.lastProcessedBlock)
 	return nil
 }
+=======
+>>>>>>> b216977 (remove unnecssary whitespace)
