db0cb4d3f59ad81dda9f96202a29c829987fa178
rapol-fantom <146690782+rapol-fantom@users.noreply.github.com>
2023-10-20 13:43:28 +0700

    aida-vm-sdb --profile --profile-file works (#747)
    * --profile flag for aida-vm-sdb now works
    
    * remove unnecssary whitespace
    
    * config -> cfg, context -> ctx
    
    * space -> tab
    
    * enabled --profile-flag and check imports

---
+++ executor/extension/profiler/operation_profiler.go
@@ -0,0 +1,37 @@
+package profiler
+
+import (
+	"github.com/Fantom-foundation/Aida/executor"
+	"github.com/Fantom-foundation/Aida/executor/extension"
+	"github.com/Fantom-foundation/Aida/state/proxy"
+	"github.com/Fantom-foundation/Aida/tracer/profile"
+	"github.com/Fantom-foundation/Aida/utils"
+)
+
+// MakeOperationProfiler creates a executor.Extension that records Operation profiling
+func MakeOperationProfiler[T any](cfg *utils.Config) executor.Extension[T] {
+	if !cfg.Profile {
+		return extension.NilExtension[T]{}
+	}
+	return &operationProfiler[T]{cfg: cfg}
+}
+
+type operationProfiler[T any] struct {
+	extension.NilExtension[T]
+	cfg   *utils.Config
+	stats *profile.Stats
+}
+
+func (p *operationProfiler[T]) PreRun(_ executor.State[T], ctx *executor.Context) error {
+	ctx.State, p.stats = proxy.NewProfilerProxy(
+		ctx.State,
+		p.cfg.ProfileFile,
+		p.cfg.LogLevel,
+	)
+	return nil
+}
+
+func (p *operationProfiler[T]) PostRun(executor.State[T], *executor.Context, error) error {
+	p.stats.PrintProfiling(p.cfg.First, p.cfg.Last)
+	return nil
+}
