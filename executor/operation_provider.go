10bc257ec9fd1641b0d2f0882c6becacf9a29efd
Petr Hanzl <petrhanzl33@gmail.com>
2023-10-20 16:02:56 +0200

    Correctly end transactions. Add condition with to block

--- executor/operation_provider.go
+++ executor/operation_provider.go
@@ -14,30 +14,59 @@ type operationProvider struct {
 
 // Run unifies operation by transactions. If operation.BeginTransactionID
 // appears current slice of operations is sent to the consumer
-func (p operationProvider) Run(from int, _ int, consumer Consumer[[]operation.Operation]) error {
+func (p operationProvider) Run(from int, to int, consumer Consumer[[]operation.Operation]) error {
 	iter := tracer.NewTraceIterator(p.traceFiles, uint64(from))
 	defer iter.Release()
 
 	tx := make([]operation.Operation, 0)
 	currentBlockNumber := from
+	var (
+		transactionNumber int
+		lastOperation     bool
+	)
 	for iter.Next() {
 		op := iter.Value()
 
-		// we need to know the block number
-		if bb, ok := op.(*operation.BeginBlock); ok {
-			currentBlockNumber = int(bb.BlockNumber)
-		}
+		// todo does this matter?
 
-		// new tx appeared - send current operation array to consumer
-		if bt, ok := op.(*operation.BeginTransaction); ok {
-			if err := consumer(TransactionInfo[[]operation.Operation]{int(bt.TransactionNumber), currentBlockNumber, tx}); err != nil {
+		// if next operation after operation.EndTransaction is operation.EndBlock append as well
+		if lastOperation {
+			var ok bool
+
+			// append operation.EndBlock as well
+			if _, ok = op.(*operation.EndBlock); ok {
+				tx = append(tx, op)
+			}
+
+			if err := consumer(TransactionInfo[[]operation.Operation]{transactionNumber, currentBlockNumber, tx}); err != nil {
 				return err
 			}
 			tx = make([]operation.Operation, 0)
+			lastOperation = false
+
+			// operation has been already appended, skip the rest of the loop
+			if ok {
+				continue
+			}
+		}
+
+		switch t := op.(type) {
+		case *operation.BeginTransaction:
+			// extract transaction number with operation.BeginTransaction
+			transactionNumber = int(t.TransactionNumber)
+		case *operation.EndTransaction:
+			// send the united operations to consumer with operation.EndTransaction
+			lastOperation = true
+		case *operation.BeginBlock:
+			// extract block number with operation.BeginBlock
+			currentBlockNumber = int(t.BlockNumber)
+			if currentBlockNumber == to {
+				return nil
+			}
+		default:
 		}
 
 		tx = append(tx, op)
-
 	}
 
 	return nil
