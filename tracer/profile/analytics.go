ae1e6f6c905165117e98a3464cce9e11cb9e140f
rapol-fantom <rapol@fantom.foundation>
2023-10-24 08:52:06 +0000

    clean with goimport

--- tracer/profile/analytics.go
+++ tracer/profile/analytics.go
@@ -1,18 +1,18 @@
 package profile
 
 import (
-	"math"
 	"encoding/json"
+	"math"
 )
 
-func min (a, b float64) float64 {
+func min(a, b float64) float64 {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-func max (a, b float64) float64 {
+func max(a, b float64) float64 {
 	if a > b {
 		return a
 	}
@@ -28,11 +28,11 @@ func NewAnalytics(opCount uint64) *Analytics {
 */
 
 type IncrementalStats struct {
-	count		uint64
-	sum		float64
-	variance	float64
-	min		float64
-	max		float64
+	count    uint64
+	sum      float64
+	variance float64
+	min      float64
+	max      float64
 }
 
 func NewIncrementalStats() *IncrementalStats {
@@ -47,17 +47,17 @@ func (s *IncrementalStats) unlessEmpty(notEmpty, empty float64) float64 {
 }
 
 func (s *IncrementalStats) Update(x float64) {
-	oldCount, newCount := float64(s.count), float64(s.count + 1)
-	oldSum, newSum := s.sum, s.sum + x
+	oldCount, newCount := float64(s.count), float64(s.count+1)
+	oldSum, newSum := s.sum, s.sum+x
 
-	oldMean := s.unlessEmpty(oldSum / oldCount, 0)
+	oldMean := s.unlessEmpty(oldSum/oldCount, 0)
 	//newMean := newSum / newCount
-	
+
 	diff := oldMean - x
 	oldVariance := s.variance
 	newVariance := s.unlessEmpty(
 		//oldVariance * (oldCount - 1) / oldCount + diffMean * diffMean / newCount,
-		(oldCount / newCount) * (oldVariance + (diff * diff / newCount)),
+		(oldCount/newCount)*(oldVariance+(diff*diff/newCount)),
 		0,
 	)
 
@@ -65,7 +65,7 @@ func (s *IncrementalStats) Update(x float64) {
 	s.sum = newSum
 	s.variance = newVariance
 	//a.min = oldCount > 0 ? min(a.min, x) : x
-	s.min = s.unlessEmpty( min(s.min, x), x)
+	s.min = s.unlessEmpty(min(s.min, x), x)
 	s.max = max(s.max, x)
 }
 
@@ -78,11 +78,11 @@ func (s *IncrementalStats) GetSum() float64 {
 }
 
 func (s *IncrementalStats) GetMean() float64 {
-	return s.unlessEmpty( s.sum/float64(s.count), 0)
+	return s.unlessEmpty(s.sum/float64(s.count), 0)
 }
 
 func (s *IncrementalStats) GetVariance() float64 {
-	return s.variance	
+	return s.variance
 }
 
 func (s *IncrementalStats) GetMin() float64 {
@@ -90,7 +90,7 @@ func (s *IncrementalStats) GetMin() float64 {
 }
 
 func (s *IncrementalStats) GetMax() float64 {
-	return s.max	
+	return s.max
 }
 
 func (s *IncrementalStats) String() string {
