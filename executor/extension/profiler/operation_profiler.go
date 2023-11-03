20023f50976b5bac4401bdb51db1a000ba7b4a3d
rapol-fantom <rapol@fantom.foundation>
2023-11-03 10:17:51 +0000

    first draft, replace stats with incremental stats implementation

--- executor/extension/profiler/operation_profiler.go
+++ executor/extension/profiler/operation_profiler.go
@@ -1,11 +1,16 @@
 package profiler
 
 import (
+	"math"
+
 	"github.com/Fantom-foundation/Aida/executor"
 	"github.com/Fantom-foundation/Aida/executor/extension"
 	"github.com/Fantom-foundation/Aida/state/proxy"
 	"github.com/Fantom-foundation/Aida/tracer/profile"
 	"github.com/Fantom-foundation/Aida/utils"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/op/go-logging"
+	"github.com/Fantom-foundation/Aida/logger"
 )
 
 // MakeOperationProfiler creates a executor.Extension that records Operation profiling
@@ -15,25 +20,64 @@ func MakeOperationProfiler[T any](cfg *utils.Config) executor.Extension[T] {
 	}
 
 	adjustedIntervalStart := cfg.First - (cfg.First % cfg.ProfileInterval)
-	return &operationProfiler[T]{
+	p := &operationProfiler[T]{
 		cfg:           cfg,
+		ps: utils.NewPrinters(),
+		log: logger.NewLogger(cfg.LogLevel, "Operation Profiler"),
 		intervalStart: cfg.First,
 		intervalEnd:   adjustedIntervalStart + cfg.ProfileInterval,
 	}
+
+	p.ps.AddPrintToConsole(func () string { return p.prettyTable().Render() })
+	p.ps.AddPrintToFile(cfg.ProfileFile, func () string { return p.prettyTable().RenderCSV() })
+
+	return p
 }
 
 type operationProfiler[T any] struct {
 	extension.NilExtension[T]
 	cfg *utils.Config
-	//stats              *profile.Stats
 	anlt               *profile.IncrementalAnalytics
+	ps	   	   *utils.Printers
+	log *logging.Logger
+
 	intervalStart      uint64
 	intervalEnd        uint64
 	lastProcessedBlock uint64
 }
 
+func (p *operationProfiler[T]) prettyTable() table.Writer {
+	t := table.NewWriter()
+	
+	totalCount := uint64(0)
+	totalSum := 0.0
+
+	t.AppendHeader(table.Row{
+		"id", "first", "last", "n", "sum(us)", "mean(us)", "std(us)", "min(us)", "max(us)",
+	})
+	for opId, stat := range p.anlt.Iterate() {
+		totalCount += stat.GetCount()
+		totalSum += stat.GetSum()
+
+		t.AppendRow(table.Row{
+			opId,
+			p.intervalStart,
+			p.intervalEnd,
+			stat.GetCount(),
+			stat.GetSum(),
+			stat.GetMean(),
+			math.Sqrt(stat.GetVariance()),
+			stat.GetMin(),
+			stat.GetMax(),
+		})
+	}
+	t.AppendFooter(table.Row{"total", "", "", totalCount, totalSum})
+	
+	return t
+}
+
 func (p *operationProfiler[T]) PreRun(_ executor.State[T], ctx *executor.Context) error {
-	ctx.State, p.stats = proxy.NewProfilerProxy(
+	ctx.State, p.anlt = proxy.NewProfilerProxy(
 		ctx.State,
 		p.cfg.ProfileFile,
 		p.cfg.LogLevel,
@@ -43,11 +87,10 @@ func (p *operationProfiler[T]) PreRun(_ executor.State[T], ctx *executor.Context
 
 func (p *operationProfiler[T]) PreBlock(state executor.State[T], _ *executor.Context) error {
 	if uint64(state.Block) > p.intervalEnd {
-		//p.stats.PrintProfiling(p.intervalStart, p.intervalEnd)
-		p.anlt.Print()
+		p.ps.Print()
 		p.intervalStart = p.intervalEnd + 1
 		p.intervalEnd = p.intervalEnd + p.cfg.ProfileInterval
-		p.stats.Reset()
+		p.anlt.Reset()
 	}
 
 	return nil
@@ -59,7 +102,6 @@ func (p *operationProfiler[T]) PostBlock(state executor.State[T], _ *executor.Co
 }
 
 func (p *operationProfiler[T]) PostRun(executor.State[T], *executor.Context, error) error {
-	//p.stats.PrintProfiling(p.intervalStart, p.lastProcessedBlock)
-	p.anlt.Print()
+	p.ps.Print()
 	return nil
 }
--- state/proxy/profiler.go
+++ state/proxy/profiler.go
@@ -19,18 +19,18 @@ import (
 // invoked StateDB operations.
 type ProfilerProxy struct {
 	db  state.StateDB  // state db
-	ps  *profile.Stats // operation statistics
+	//ps  *profile.Stats // operation statistics
+	anlt *profile.IncrementalAnalytics
 	log *logging.Logger
 }
 
 // NewProfilerProxy creates a new StateDB profiler.
-func NewProfilerProxy(db state.StateDB, csv string, logLevel string) (*ProfilerProxy, *profile.Stats) {
+func NewProfilerProxy(db state.StateDB, csv string, logLevel string) (*ProfilerProxy, *profile.IncrementalAnalytics) {
 	p := new(ProfilerProxy)
 	p.db = db
-	p.ps = profile.NewStats(csv)
-	p.ps.FillLabels(operation.CreateIdLabelMap())
+	p.anlt = profile.NewIncrementalAnalytics(len(operation.CreateIdLabelMap()))
 	p.log = logger.NewLogger(logLevel, "Proxy Profiler")
-	return p, p.ps
+	return p, p.anlt
 }
 
 // CreateAccount creates a new account.
@@ -272,7 +272,7 @@ func (p *ProfilerProxy) do(opId byte, op func()) {
 	start := time.Now()
 	op()
 	elapsed := time.Since(start)
-	p.ps.Profile(opId, elapsed)
+	p.anlt.Update(opId, float64(elapsed))
 }
 
 func (p *ProfilerProxy) BeginTransaction(number uint32) {
