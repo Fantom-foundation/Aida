0e192894067332c995c7d58c2c541e62f68fffce
Petr Hanzl <petrhanzl33@gmail.com>
2023-11-22 13:22:07 +0100

    Add priming test.

--- executor/extension/primer/primer.go
+++ executor/extension/primer/primer.go
@@ -50,11 +50,11 @@ func (p *stateDbPrimer[T]) PreRun(_ executor.State[T], ctx *executor.Context) er
 		p.log.Infof("Randomized Priming enabled; Seed: %v, threshold: %v", p.cfg.RandomSeed, p.cfg.PrimeThreshold)
 	}
 
-	p.log.Infof("Update buffer size: %v bytes", p.cfg.UpdateBufferSize)
-
 	if p.cfg.First == 0 {
 		return nil
 	}
+
+	p.log.Infof("Update buffer size: %v bytes", p.cfg.UpdateBufferSize)
 
 	p.log.Noticef("Priming to block %v...", p.cfg.First-1)
 	p.ctx = utils.NewPrimeContext(p.cfg, ctx.State, p.log)
