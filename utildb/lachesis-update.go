0d63400c6e1c72b15ca22558f5f5f4cfb00ce0b8
Petr Hanzl <petrhanzl33@gmail.com>
2024-01-16 21:12:51 +0700

    Fix more code

--- utildb/lachesis-update.go
+++ utildb/lachesis-update.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/Fantom-foundation/Aida/executor/transaction"
 	"github.com/Fantom-foundation/Aida/utils"
 	"github.com/Fantom-foundation/Aida/world-state/db/snapshot"
 	substate "github.com/Fantom-foundation/Substate"
@@ -32,7 +33,7 @@ func CreateLachesisWorldState(cfg *utils.Config) (substate.SubstateAlloc, error)
 		return nil, err
 	}
 	// remove deleted accounts
-	if err := utils.DeleteDestroyedAccountsFromWorldState(lachesis, cfg, lachesisLastBlock); err != nil {
+	if err := utils.DeleteDestroyedAccountsFromWorldState(transaction.NewOldSubstateAlloc(lachesis), cfg, lachesisLastBlock); err != nil {
 		return nil, err
 	}
 	return lachesis, nil
