cecc3bcd839943320770a201678595130f213cd2
rapol-fantom <rapol@fantom.foundation>
2024-01-17 07:00:45 +0000

    remove all pathing away from gen_processing_reports.sh

--- scripts/analytics/rmd/gen_processing_reports.sh
+++ scripts/analytics/rmd/gen_processing_reports.sh
@@ -3,23 +3,25 @@
 #    gen_processing_reports.sh -  script for generating the f1 reports
 #
 # Synopsis: 
-#    gen_processing_report.sh <output-dir>
+#    gen_processing_report.sh <path-to-rmd> <path-to-sqlite3-db> <output-dir>
 #
 # Description: 
 #    Produces f1 reports in the HTML format.
 #
 
 # check the number of command line arguments
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Invalid number of command line arguments supplied"
     exit 1
 fi
 
 # assign variables for command line arguments
-outputdir=$1
+db=$1
+rmd=$2
+outputdir=$3
+
 
 log "render f1 report ..."
-/home/rapolt/dev/aida/scripts/analytics/rmd/knit.R \
-                 -d /var/opera/Aida/tmp-rapolt/register/s5-f1-test.db \
-		 -f html -o test.html \
-		 -O $outputdir /home/rapolt/dev/aida/scripts/analytics/rmd/f1.rmd
+./knit.R -d $db \
+	 -f html -o f1.html \
+	 -O $outputdir $rmd
