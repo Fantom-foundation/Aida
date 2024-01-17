185ac8a091e6453c5f966498be2ff8ac48daf851
rapol-fantom <rapol@fantom.foundation>
2024-01-17 07:57:40 +0000

    also make knit.R relative

--- scripts/analytics/rmd/gen_processing_reports.sh
+++ scripts/analytics/rmd/gen_processing_reports.sh
@@ -3,22 +3,22 @@
 #    gen_processing_reports.sh -  script for generating the f1 reports
 #
 # Synopsis: 
-#    gen_processing_report.sh <path-to-rmd> <path-to-sqlite3-db> <output-dir>
+#    gen_processing_report.sh <path-to-knit> <path-to-rmd> <path-to-sqlite3-db> <output-dir>
 #
 # Description: 
 #    Produces f1 reports in the HTML format.
 #
 
 # check the number of command line arguments
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
     echo "Invalid number of command line arguments supplied"
     exit 1
 fi
 
 # assign variables for command line arguments
-db=$1
-rmd=$2
-outputdir=$3
+knitr=$1
+db=$2
+rmd=$3
+outputdir=$4
 
-log "render f1 report ..."
-./knit.R -d $db -f html -o f1.html -O $outputdir $rmd
+$knitr -d $db -f html -o f1.html -O $outputdir $rmd
