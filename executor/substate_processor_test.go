8dcf6876586ad6962976584b3b1ac01bcc255c0c
Petr Hanzl <petrhanzl33@gmail.com>
2024-01-19 11:44:57 +0700

    Pass hash error in test

--- executor/substate_processor_test.go
+++ executor/substate_processor_test.go
@@ -30,8 +30,9 @@ func TestPrepareBlockCtx(t *testing.T) {
 	basefee := big.NewInt(12345)
 	env := substatecontext.NewBlockEnvironment(&substate.SubstateEnv{Difficulty: big.NewInt(1), GasLimit: gaslimit, Number: blocknum, Timestamp: 1675961395, BaseFee: basefee})
 
+	var hashError error
 	// BlockHashes are nil, expect an error
-	blockCtx := prepareBlockCtx(env)
+	blockCtx := prepareBlockCtx(env, &hashError)
 
 	if blocknum != blockCtx.BlockNumber.Uint64() {
 		t.Fatalf("Wrong block number")
@@ -41,6 +42,9 @@ func TestPrepareBlockCtx(t *testing.T) {
 	}
 	if basefee.Cmp(blockCtx.BaseFee) != 0 {
 		t.Fatalf("Wrong base fee")
+	}
+	if hashError != nil {
+		t.Fatalf("Hash error; %v", hashError)
 	}
 }
 
