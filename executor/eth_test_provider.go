5b0718e044a6da80142136039adb466d675d1b76
Petr Hanzl <petrhanzl33@gmail.com>
2024-02-14 12:12:10 +0700

    assign correct numbers for blk and tx in the provider

--- executor/eth_test_provider.go
+++ executor/eth_test_provider.go
@@ -1,8 +1,6 @@
 package executor
 
 import (
-	"fmt"
-
 	statetest "github.com/Fantom-foundation/Aida/ethtest"
 	blocktest "github.com/Fantom-foundation/Aida/ethtest/block_test"
 	"github.com/Fantom-foundation/Aida/txcontext"
@@ -23,19 +21,20 @@ func (e ethTestProvider) Run(_ int, _ int, consumer Consumer[txcontext.TxContext
 		return err
 	}
 
+	var blkCount int
 	// iterate all tests
 	for _, bt := range b {
 		// iterate blocks inside tests
-		for blockCounter, blk := range bt.Blocks {
+		for _, blk := range bt.Blocks {
+			blkCount++
 			// iterate txs inside block
-			for txCounter, tx := range blk.Transactions {
+			for txCount, tx := range blk.Transactions {
 				err = consumer(TransactionInfo[txcontext.TxContext]{
-					Block:       blockCounter + 1, // zero block is genesis
-					Transaction: txCounter,
+					Block:       blkCount + 1, // zero block is genesis
+					Transaction: txCount,
 					Data:        blocktest.NewData(blk, tx, bt),
 				})
 			}
-			fmt.Println(blockCounter)
 		}
 
 	}
@@ -44,7 +43,7 @@ func (e ethTestProvider) Run(_ int, _ int, consumer Consumer[txcontext.TxContext
 	//for _, t := range b {
 	//	// divide them by fork
 	//	for i, dt := range t.Divide(e.cfg.ChainID) {
-	//		err = consumer(TransactionInfo[txcontext.TxContext]{
+	//		err = consumer(TransactionInfo[txcontext.txContext]{
 	//			Block:       int(dt.env.GetNumber()),
 	//			Transaction: i,
 	//			Data:        dt,
