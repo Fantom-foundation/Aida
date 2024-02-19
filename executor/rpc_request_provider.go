0c994fa9bb5b425a350c56af8c5233c751012b2e
Petr Hanzl <petrhanzl33@gmail.com>
2024-02-19 13:22:19 +0700

    Report about skipping blocks. Report about no request in iterator. Decode block numbers and timestamp in provider. Add retry for any mismatched requests.

--- executor/rpc_request_provider.go
+++ executor/rpc_request_provider.go
@@ -1,8 +1,10 @@
 package executor
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/Fantom-foundation/Aida/logger"
 	"github.com/Fantom-foundation/Aida/rpc"
 	"github.com/Fantom-foundation/Aida/utils"
 	"github.com/urfave/cli/v2"
@@ -21,6 +23,7 @@ func openRpcRecording(iter rpc.Iterator, cfg *utils.Config, ctxt *cli.Context) P
 		ctxt:     ctxt,
 		fileName: cfg.RpcRecordingFile,
 		iter:     iter,
+		log:      logger.NewLogger(cfg.LogLevel, "rpc-provider"),
 	}
 }
 
@@ -28,10 +31,13 @@ type rpcRequestProvider struct {
 	ctxt     *cli.Context
 	fileName string
 	iter     rpc.Iterator
+	log      logger.Logger
 }
 
 func (r rpcRequestProvider) Run(from int, to int, consumer Consumer[*rpc.RequestAndResults]) error {
-	var blockNumber int
+	if err := r.processFirst(from, consumer); err != nil {
+		return err
+	}
 
 	for r.iter.Next() {
 		if r.iter.Error() != nil {
@@ -41,7 +47,7 @@ func (r rpcRequestProvider) Run(from int, to int, consumer Consumer[*rpc.Request
 		req := r.iter.Value()
 
 		if req == nil {
-			return nil
+			return errors.New("iterator returned nil request")
 		}
 
 		// get logs is not yet implemented, skip these for now
@@ -49,22 +55,18 @@ func (r rpcRequestProvider) Run(from int, to int, consumer Consumer[*rpc.Request
 			continue
 		}
 
-		if req.Response != nil {
-			blockNumber = int(req.Response.BlockID)
-		} else {
-			blockNumber = int(req.Error.BlockID)
-		}
+		req.DecodeInfo()
 
 		// are we skipping requests?
-		if blockNumber < from {
+		if req.RecordedBlock < from {
 			continue
 		}
 
-		if blockNumber >= to {
+		if req.RecordedBlock >= to {
 			return nil
 		}
 
-		if err := consumer(TransactionInfo[*rpc.RequestAndResults]{blockNumber, 0, req}); err != nil {
+		if err := consumer(TransactionInfo[*rpc.RequestAndResults]{req.RecordedBlock, 0, req}); err != nil {
 			return err
 		}
 	}
@@ -74,4 +76,41 @@ func (r rpcRequestProvider) Run(from int, to int, consumer Consumer[*rpc.Request
 
 func (r rpcRequestProvider) Close() {
 	r.iter.Close()
+}
+
+// processFirst takes first request and logs information about run.
+func (r rpcRequestProvider) processFirst(from int, consumer Consumer[*rpc.RequestAndResults]) error {
+	if r.iter.Next() {
+		if r.iter.Error() != nil {
+			return fmt.Errorf("iterator returned error; %v", r.iter.Error())
+		}
+
+		req := r.iter.Value()
+		if req == nil {
+			return errors.New("iterator returned nil request")
+		}
+
+		req.DecodeInfo()
+		r.log.Noticef("First block of recording: %v", req.RecordedBlock)
+
+		// are we skipping requests?
+		if req.RecordedBlock < from {
+			r.log.Noticef("Skipping %v blocks.\nThis might take a while, skip rate is ~50k Req/s "+
+				"and there is up to 2500 Requests in a block.", from-req.RecordedBlock)
+			return nil
+		}
+
+		// get logs is not yet implemented, skip these for now
+		if req.Query.MethodBase == "getLogs" {
+			return nil
+		}
+
+		if err := consumer(TransactionInfo[*rpc.RequestAndResults]{req.RecordedBlock, 0, req}); err != nil {
+			return err
+		}
+	} else {
+		r.log.Critical("Iterator returned no requests.")
+	}
+
+	return nil
 }
--- rpc/request.go
+++ rpc/request.go
@@ -1,16 +1,58 @@
 package rpc
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
 
 // RequestAndResults encapsulates request query and response for post-processing.
 type RequestAndResults struct {
-	Query          *Body
-	Response       *Response
-	Error          *ErrorResponse
-	ParamsRaw      []byte
-	ResponseRaw    []byte
-	StateDB        *StateDBData
-	SkipValidation bool
+	Query                                    *Body
+	Response                                 *Response
+	Error                                    *ErrorResponse
+	ParamsRaw                                []byte
+	ResponseRaw                              []byte
+	StateDB                                  *StateDBData
+	SkipValidation                           bool
+	RecordedBlock, RequestedBlock, Timestamp int
+}
+
+// DecodeInfo finds recorded and requested block numbers as well as timestamp of the recorded block.
+func (r *RequestAndResults) DecodeInfo() {
+	if r.Response != nil {
+		r.RecordedBlock = int(r.Response.BlockID)
+		r.Timestamp = int(r.Response.Timestamp)
+	} else {
+		r.RecordedBlock = int(r.Error.BlockID)
+		r.Timestamp = int(r.Error.Timestamp)
+	}
+	r.findRequestedBlock()
+}
+
+func (r *RequestAndResults) findRequestedBlock() {
+	l := len(r.Query.Params)
+	if l < 2 {
+		r.RequestedBlock = r.RecordedBlock
+		return
+	}
+
+	str := r.Query.Params[l-1].(string)
+	switch str {
+	case "pending":
+		// validation for pending requests does not work, skip them
+		r.SkipValidation = true
+		// pending should be treated as latest
+		fallthrough
+	case "latest":
+		r.RequestedBlock = r.RecordedBlock
+	case "earliest":
+		r.RequestedBlock = 0
+
+	default:
+		// botched params are not recorded, so this will  never panic
+		r.RequestedBlock = int(hexutil.MustDecodeUint64(str))
+	}
 }
 
 // Body represents a decoded payload of a balancer.
