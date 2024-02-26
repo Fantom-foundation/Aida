0f6fd5800c1dd1967fea27790bce9ac816f69640
Herbert Jordan <herbert@swrnd.ch>
2024-02-26 13:35:17 +0100

    Add comment on ordering of extensions

--- cmd/aida-vm-sdb/run_eth.go
+++ cmd/aida-vm-sdb/run_eth.go
@@ -92,7 +92,7 @@ func runEth(
 			statedb.MakeEthStateTestDbPrepper(cfg),
 			statedb.MakeLiveDbBlockChecker[txcontext.TxContext](cfg),
 			logger.MakeDbLogger[txcontext.TxContext](cfg),
-			statedb.MakeEthStateTestDbPrimer(cfg),
+			statedb.MakeEthStateTestDbPrimer(cfg), // < to be placed after the DbLogger to log priming operations
 		)
 	}
 
