5bf9946e27bb142f3598aff531bfb7b0ef3d6bc7
Bernhard Scholz <bernhard@fantom.foundation>
2024-04-15 21:21:04 +0200

    Add short headers for the LGPL 3.0 license

--- executor/extension/logger/eth_logger.go
+++ executor/extension/logger/eth_logger.go
@@ -1,3 +1,19 @@
+// Copyright 2024 Fantom Foundation
+// This file is part of Aida Testing Infrastructure for Sonic
+//
+// Aida is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Aida is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Aida. If not, see <http://www.gnu.org/licenses/>.
+
 package logger
 
 import (
