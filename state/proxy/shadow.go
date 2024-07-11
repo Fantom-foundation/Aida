642d3f289631ea433a7fa161accfbbb853c16aa4
Petr Hanzl <petrhanzl33@gmail.com>
2024-07-11 09:36:00 +0200

    Remove unnecessary condition

--- state/proxy/shadow.go
+++ state/proxy/shadow.go
@@ -714,13 +714,8 @@ func (s *shadowVmStateDb) Selfdestruct6780(addr common.Address) {
 }
 
 func (s *shadowVmStateDb) GetStorageRoot(addr common.Address) common.Hash {
-	// todo call both return non-empty
 	primeRoot := s.prime.GetStorageRoot(addr)
 	shadowRoot := s.shadow.GetStorageRoot(addr)
-
-	if primeRoot == shadowRoot {
-		return primeRoot
-	}
 
 	if shadowRoot != (common.Hash{}) {
 		return shadowRoot
